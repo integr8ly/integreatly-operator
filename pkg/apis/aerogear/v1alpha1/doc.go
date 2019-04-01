15f706b6f049c3b000468e790149e306d7f30a7c
Phil Brookes <phil.brookes@gmail.com>
2019-04-01 16:22:21 +0100

    codebase for integreatly-operator

---
+++ pkg/apis/aerogear/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the aerogear v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=aerogear.org
+package v1alpha1
---
+++ pkg/controller/installation/installation_controller.go
@@ -0,0 +1,85 @@
+package installation
+
+import (
+	"context"
+
+	aerogearv1alpha1 "github.com/integr8ly/integreatly-operator/pkg/apis/aerogear/v1alpha1"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller_installation")
+
+
+// Add creates a new Installation Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileInstallation{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("installation-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource Installation
+	err = c.Watch(&source.Kind{Type: &aerogearv1alpha1.Installation{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to secondary resource Pods and requeue the owner Installation
+	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &aerogearv1alpha1.Installation{},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileInstallation{}
+
+// ReconcileInstallation reconciles a Installation object
+type ReconcileInstallation struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a Installation object and makes changes based on the state read
+// and what is in the Installation.Spec
+func (r *ReconcileInstallation) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	instance := &aerogearv1alpha1.Installation{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, err
+	}
+
+	//TODO: installation reconciliation goes here
+
+	return reconcile.Result{}, nil
+}
