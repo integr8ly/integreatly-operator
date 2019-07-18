50b5ca541ccddc35fae7bc0bd3fe5346649559ac
Jameel Briones <jbriones@redhat.com>
2019-07-18 11:34:41 +0100

    add github client id and secret to installation cr

--- pkg/apis/integreatly/v1alpha1/installation_types.go
+++ pkg/apis/integreatly/v1alpha1/installation_types.go
@@ -35,9 +35,11 @@ type InstallationSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
-	Type            string `json:"type"`
-	NamespacePrefix string `json:"namespacePrefix"`
-	SelfSignedCerts bool   `json:"selfSignedCerts"`
+	Type                    string `json:"type"`
+	NamespacePrefix         string `json:"namespacePrefix"`
+	SelfSignedCerts         bool   `json:"selfSignedCerts"`
+	GithubOauthClientId     string `json:"githubOauthClientId"`
+	GithubOauthClientSecret string `json:"githubOauthClientSecret"`
 }
 
 // InstallationStatus defines the observed state of Installation
--- pkg/controller/installation/products/launcher/reconciler.go
+++ pkg/controller/installation/products/launcher/reconciler.go
@@ -1,37 +1,53 @@
 package launcher
 
 import (
-	"context"
-	"errors"
 	"fmt"
-	aerogearv1 "github.com/integr8ly/integreatly-operator/pkg/apis/aerogear/v1alpha1"
+
 	"github.com/integr8ly/integreatly-operator/pkg/apis/integreatly/v1alpha1"
 	"github.com/integr8ly/integreatly-operator/pkg/controller/installation/marketplace"
 	"github.com/integr8ly/integreatly-operator/pkg/controller/installation/products/config"
-	coreosv1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
-	pkgerr "github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	"k8s.io/api/core/v1"
-	k8serr "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	pkgclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+const (
+	defaultInstallationNamespace = "launcher"
+)
+
+type Reconciler struct {
+	coreClient    kubernetes.Interface
+	Config        *config.Launcher
+	ConfigManager config.ConfigReadWriter
+	mpm           marketplace.MarketplaceInterface
+	logger        *logrus.Entry
+}
 
 func NewReconciler(coreClient *kubernetes.Clientset, configManager config.ConfigReadWriter, instance *v1alpha1.Installation, mpm marketplace.MarketplaceInterface) (*Reconciler, error) {
 	config, err := configManager.ReadLauncher()
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println("LAUNCHER NAMESPACE: ", config.GetNamespace())
-	// fmt config.GetNamespace()
-	// if config.GetNamespace() == "" {
-	// 	config.SetNamespace(instance.Spec.NamespacePrefix + defaultInstallationNamespace)
-	// }
+
+	if config.GetNamespace() == "" {
+		config.SetNamespace(instance.Spec.NamespacePrefix + defaultInstallationNamespace)
+	}
+
 	return &Reconciler{
 		coreClient:    coreClient,
 		ConfigManager: configManager,
 		Config:        config,
 		mpm:           mpm,
 	}, nil
+}
+
+func (r *Reconciler) Reconcile(inst *v1alpha1.Installation, serverClient pkgclient.Client) (v1alpha1.StatusPhase, error) {
+	fmt.Println("Reconciling", inst.Spec)
+
+	// TODO Need to add github_client_secret and github_client_id to the Installation CR
+
+	// TODO Create launcher_oauth_github Secret
+
+	// OauthClient
+	return v1alpha1.PhaseInProgress, nil
 }
