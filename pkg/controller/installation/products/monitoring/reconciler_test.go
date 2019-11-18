0a4c81ec159e4de44977f254b1c65b526265e2a6
Steven Tobin <stobin@redhat.com>
2019-11-18 13:45:41 +0000

    INTLY-4153 Add unit tests for monitoring reconciler

---
+++ pkg/controller/installation/products/monitoring/reconciler_test.go
@@ -0,0 +1,392 @@
+package monitoring
+
+import (
+	"context"
+	"github.com/integr8ly/integreatly-operator/pkg/resources"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/integr8ly/integreatly-operator/pkg/controller/installation/products/config"
+	"github.com/operator-framework/operator-lifecycle-manager/pkg/lib/ownerutil"
+
+	"testing"
+
+	"github.com/integr8ly/integreatly-operator/pkg/apis/integreatly/v1alpha1"
+	integreatlyv1alpha1 "github.com/integr8ly/integreatly-operator/pkg/apis/integreatly/v1alpha1"
+	monitoringv1 "github.com/integr8ly/integreatly-operator/pkg/apis/monitoring/v1alpha1"
+	moqclient "github.com/integr8ly/integreatly-operator/pkg/client"
+	"github.com/integr8ly/integreatly-operator/pkg/controller/installation/marketplace"
+	coreosv1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1"
+	operatorsv1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
+	marketplacev1 "github.com/operator-framework/operator-marketplace/pkg/apis/operators/v1"
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	k8serr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	pkgclient "sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func basicInstallation() *v1alpha1.Installation {
+	return &v1alpha1.Installation{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "installation",
+			Namespace: defaultInstallationNamespace,
+			UID:       types.UID("xyz"),
+		},
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "installation",
+			APIVersion: v1alpha1.SchemeGroupVersion.String(),
+		},
+	}
+}
+
+func basicConfigMock() *config.ConfigReadWriterMock {
+	return &config.ConfigReadWriterMock{
+		ReadMonitoringFunc: func() (ready *config.Monitoring, e error) {
+			return config.NewMonitoring(config.ProductConfig{}), nil
+		},
+		WriteConfigFunc: func(config config.ConfigReadable) error {
+			return nil
+		},
+	}
+}
+
+func getBuildScheme() (*runtime.Scheme, error) {
+	scheme := runtime.NewScheme()
+	err := monitoringv1.SchemeBuilder.AddToScheme(scheme)
+	err = integreatlyv1alpha1.SchemeBuilder.AddToScheme(scheme)
+	err = operatorsv1alpha1.AddToScheme(scheme)
+	err = marketplacev1.SchemeBuilder.AddToScheme(scheme)
+	err = corev1.SchemeBuilder.AddToScheme(scheme)
+	err = coreosv1.SchemeBuilder.AddToScheme(scheme)
+	return scheme, err
+}
+
+func TestReconciler_config(t *testing.T) {
+	cases := []struct {
+		Name           string
+		ExpectError    bool
+		ExpectedStatus v1alpha1.StatusPhase
+		ExpectedError  string
+		FakeConfig     *config.ConfigReadWriterMock
+		FakeClient     pkgclient.Client
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+		Installation   *v1alpha1.Installation
+	}{
+		{
+			Name:           "test error on failed config",
+			ExpectedStatus: v1alpha1.PhaseFailed,
+			ExpectError:    true,
+			ExpectedError:  "could not read monitoring config",
+			Installation:   &v1alpha1.Installation{},
+			FakeClient:     fakeclient.NewFakeClient(),
+			FakeConfig: &config.ConfigReadWriterMock{
+				ReadMonitoringFunc: func() (ready *config.Monitoring, e error) {
+					return nil, errors.New("could not read monitoring config")
+				},
+			},
+		},
+		{
+			Name:         "test namespace is set without fail",
+			Installation: &v1alpha1.Installation{},
+			FakeClient:   fakeclient.NewFakeClient(),
+			FakeConfig: &config.ConfigReadWriterMock{
+				ReadMonitoringFunc: func() (ready *config.Monitoring, e error) {
+					return config.NewMonitoring(config.ProductConfig{
+						"NAMESPACE": "",
+					}), nil
+				},
+			},
+		},
+		{
+			Name:           "test subscription phase with error from mpm",
+			ExpectedStatus: v1alpha1.PhaseFailed,
+			ExpectError:    true,
+			Installation:   &v1alpha1.Installation{},
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return errors.New("dummy")
+				},
+			},
+			FakeClient: fakeclient.NewFakeClient(),
+			FakeConfig: basicConfigMock(),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			_, err := NewReconciler(tc.FakeConfig, tc.Installation, tc.FakeMPM)
+			if err != nil && err.Error() != tc.ExpectedError {
+				t.Fatalf("unexpected error : '%v', expected: '%v'", err, tc.ExpectedError)
+			}
+			if err == nil && tc.ExpectedError != "" {
+				t.Fatalf("expected error '%v' and got nil", tc.ExpectedError)
+			}
+		})
+	}
+
+}
+
+func TestReconciler_reconcileCustomResource(t *testing.T) {
+	scheme, err := getBuildScheme()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cases := []struct {
+		Name           string
+		FakeClient     pkgclient.Client
+		FakeConfig     *config.ConfigReadWriterMock
+		Installation   *v1alpha1.Installation
+		ExpectError    bool
+		ExpectedError  string
+		ExpectedStatus v1alpha1.StatusPhase
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+	}{
+		{
+			Name:           "Test reconcile custom resource returns success on successful create",
+			FakeClient:     fakeclient.NewFakeClientWithScheme(scheme),
+			FakeConfig:     basicConfigMock(),
+			Installation:   &v1alpha1.Installation{},
+			ExpectedStatus: v1alpha1.PhaseCompleted,
+		},
+		{
+			Name: "Test reconcile custom resource returns failed on unsuccessful create",
+			FakeClient: &moqclient.SigsClientInterfaceMock{
+				GetFunc: func(ctx context.Context, key types.NamespacedName, obj runtime.Object) error {
+					return k8serr.NewNotFound(schema.GroupResource{
+						Group:    monitoringv1.SchemeBuilder.GroupVersion.Group,
+						Resource: "ApplicationMonitoring",
+					}, key.Name)
+				},
+				CreateFunc: func(ctx context.Context, obj runtime.Object) error {
+					return errors.New("dummy create error")
+				},
+			},
+			FakeConfig:     basicConfigMock(),
+			Installation:   &v1alpha1.Installation{},
+			ExpectedStatus: v1alpha1.PhaseFailed,
+			ExpectError:    true,
+			ExpectedError:  "failed to create/update applicationmonitoring custom resource",
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			reconciler, err := NewReconciler(tc.FakeConfig, tc.Installation, tc.FakeMPM)
+			if err != nil {
+				t.Fatal("unexpected err ", err)
+			}
+
+			phase, err := reconciler.reconcileComponents(context.TODO(), tc.Installation, tc.FakeClient)
+			if tc.ExpectError && err == nil {
+				t.Fatal("expected an error but got none")
+			}
+			if !tc.ExpectError && err != nil {
+				t.Fatal("expected no error but got one ", err)
+			}
+			if tc.ExpectedStatus != phase {
+				t.Fatal("expected phase ", tc.ExpectedStatus, " but got ", phase)
+			}
+		})
+	}
+}
+
+func TestReconciler_fullReconcile(t *testing.T) {
+	scheme, err := getBuildScheme()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// initialise runtime objects
+	namespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: defaultInstallationNamespace,
+			Labels: map[string]string{
+				resources.OwnerLabelKey: string(basicInstallation().GetUID()),
+			},
+		},
+		Status: corev1.NamespaceStatus{
+			Phase: corev1.NamespaceActive,
+		},
+	}
+	cases := []struct {
+		Name           string
+		ExpectError    bool
+		ExpectedStatus v1alpha1.StatusPhase
+		ExpectedError  string
+		FakeConfig     *config.ConfigReadWriterMock
+		FakeClient     pkgclient.Client
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+		Installation   *v1alpha1.Installation
+		Product        *v1alpha1.InstallationProductStatus
+	}{
+		{
+			Name:           "test successful reconcile",
+			ExpectedStatus: v1alpha1.PhaseCompleted,
+			FakeClient:     moqclient.NewSigsClientMoqWithScheme(scheme, namespace, basicInstallation()),
+			FakeConfig: &config.ConfigReadWriterMock{
+				ReadMonitoringFunc: func() (ready *config.Monitoring, e error) {
+					return config.NewMonitoring(config.ProductConfig{
+						"NAMESPACE": "",
+					}), nil
+				},
+			},
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return nil
+				},
+				GetSubscriptionInstallPlansFunc: func(ctx context.Context, serverClient pkgclient.Client, subName string, ns string) (plan *operatorsv1alpha1.InstallPlanList, subscription *operatorsv1alpha1.Subscription, e error) {
+					return &operatorsv1alpha1.InstallPlanList{
+							TypeMeta: metav1.TypeMeta{
+								Kind:       "ApplicationMonitoring",
+								APIVersion: monitoringv1.SchemeGroupVersion.String(),
+							},
+							Items: []operatorsv1alpha1.InstallPlan{
+								{
+									ObjectMeta: metav1.ObjectMeta{
+										Name: "monitoring-install-plan",
+									},
+									Status: operatorsv1alpha1.InstallPlanStatus{
+										Phase: operatorsv1alpha1.InstallPlanPhaseComplete,
+									},
+								},
+							},
+							ListMeta: metav1.ListMeta{},
+						}, &operatorsv1alpha1.Subscription{
+							Status: operatorsv1alpha1.SubscriptionStatus{
+								Install: &operatorsv1alpha1.InstallPlanReference{
+									Name: "monitoring-install-plan",
+								},
+							},
+						}, nil
+				},
+			},
+			Installation: basicInstallation(),
+			Product:      &v1alpha1.InstallationProductStatus{},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			reconciler, err := NewReconciler(tc.FakeConfig, tc.Installation, tc.FakeMPM)
+			if err != nil && err.Error() != tc.ExpectedError {
+				t.Fatalf("unexpected error : '%v', expected: '%v'", err, tc.ExpectedError)
+			}
+
+			status, err := reconciler.Reconcile(context.TODO(), tc.Installation, tc.Product, tc.FakeClient)
+			if err != nil && !tc.ExpectError {
+				t.Fatalf("expected error but got one: %v", err)
+			}
+			if err == nil && tc.ExpectError {
+				t.Fatal("expected error but got none")
+			}
+			if status != tc.ExpectedStatus {
+				t.Fatalf("Expected status: '%v', got: '%v'", tc.ExpectedStatus, status)
+			}
+		})
+	}
+}
+
+func TestReconciler_testPhases(t *testing.T) {
+	scheme, err := getBuildScheme()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cases := []struct {
+		Name           string
+		ExpectedStatus v1alpha1.StatusPhase
+		FakeConfig     *config.ConfigReadWriterMock
+		FakeClient     pkgclient.Client
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+		Installation   *v1alpha1.Installation
+		Product        *v1alpha1.InstallationProductStatus
+	}{
+		{
+			Name:           "test namespace terminating returns phase in progress",
+			ExpectedStatus: v1alpha1.PhaseInProgress,
+			Installation:   basicInstallation(),
+			FakeClient: moqclient.NewSigsClientMoqWithScheme(scheme, &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: defaultInstallationNamespace,
+				},
+				Status: corev1.NamespaceStatus{
+					Phase: corev1.NamespaceTerminating,
+				},
+			}, basicInstallation()),
+			FakeConfig: basicConfigMock(),
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return nil
+				},
+				GetSubscriptionInstallPlansFunc: func(ctx context.Context, serverClient pkgclient.Client, subName string, ns string) (plan *operatorsv1alpha1.InstallPlanList, subscription *operatorsv1alpha1.Subscription, e error) {
+					return &operatorsv1alpha1.InstallPlanList{}, &operatorsv1alpha1.Subscription{}, nil
+				},
+			},
+			Product: &v1alpha1.InstallationProductStatus{},
+		},
+		{
+			Name:           "test subscription creating returns phase in progress",
+			ExpectedStatus: v1alpha1.PhaseInProgress,
+			Installation:   basicInstallation(),
+			FakeClient:     moqclient.NewSigsClientMoqWithScheme(scheme, basicInstallation()),
+			FakeConfig:     basicConfigMock(),
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return nil
+				},
+				GetSubscriptionInstallPlansFunc: func(ctx context.Context, serverClient pkgclient.Client, subName string, ns string) (*operatorsv1alpha1.InstallPlanList, *operatorsv1alpha1.Subscription, error) {
+					return &operatorsv1alpha1.InstallPlanList{}, &operatorsv1alpha1.Subscription{}, nil
+				},
+			},
+			Product: &v1alpha1.InstallationProductStatus{},
+		},
+		{
+			Name:           "test components creating returns phase in progress",
+			ExpectedStatus: v1alpha1.PhaseInProgress,
+			Installation:   basicInstallation(),
+			FakeClient:     moqclient.NewSigsClientMoqWithScheme(scheme, basicInstallation()),
+			FakeConfig:     basicConfigMock(),
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient pkgclient.Client, owner ownerutil.Owner, os marketplacev1.OperatorSource, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return nil
+				},
+				GetSubscriptionInstallPlansFunc: func(ctx context.Context, serverClient pkgclient.Client, sub string, ns string) (*operatorsv1alpha1.InstallPlanList, *operatorsv1alpha1.Subscription, error) {
+					return &operatorsv1alpha1.InstallPlanList{
+							TypeMeta: metav1.TypeMeta{
+								Kind:       "ApplicationMonitoring",
+								APIVersion: monitoringv1.SchemeGroupVersion.String(),
+							},
+							ListMeta: metav1.ListMeta{},
+						}, &operatorsv1alpha1.Subscription{
+							Status: operatorsv1alpha1.SubscriptionStatus{
+								Install: &operatorsv1alpha1.InstallPlanReference{
+									Name: "monitoring-install-plan",
+								},
+							},
+						}, nil
+				},
+			},
+			Product: &v1alpha1.InstallationProductStatus{},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			reconciler, err := NewReconciler(tc.FakeConfig, tc.Installation, tc.FakeMPM)
+			if err != nil {
+				t.Fatalf("unexpected error : '%v'", err)
+			}
+
+			status, err := reconciler.Reconcile(context.TODO(), tc.Installation, tc.Product, tc.FakeClient)
+			if err != nil {
+				t.Fatalf("expected no error but got one: %v", err)
+			}
+			if status != tc.ExpectedStatus {
+				t.Fatalf("Expected status: '%v', got: '%v'", tc.ExpectedStatus, status)
+			}
+		})
+	}
+}
