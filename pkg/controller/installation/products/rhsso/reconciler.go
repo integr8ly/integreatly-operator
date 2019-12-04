c554746b0909cb756b6e14207d1d010f31c5e130
David Ffrench <davidffrench@gmail.com>
2019-12-04 16:20:27 +0000

    fix: fix keycloak route not working

--- pkg/controller/installation/products/rhsso/reconciler.go
+++ pkg/controller/installation/products/rhsso/reconciler.go
@@ -265,17 +265,21 @@ func (r *Reconciler) createKeycloakRoute(ctx context.Context, inst *v1alpha1.Ins
 	r.logger.Info(fmt.Sprintf("operation result of creating %v service was %v", httpService.Name, or))
 
 	or, err = controllerutil.CreateOrUpdate(ctx, serverClient, edgeRoute, func() error {
-		edgeRoute.Spec.To = v12.RouteTargetReference{
-			Kind: "Service",
-			Name: "keycloak-http",
+		host := edgeRoute.Spec.Host
+		edgeRoute.Spec = v12.RouteSpec{
+			Host: host,
+			To: v12.RouteTargetReference{
+				Kind: "Service",
+				Name: "keycloak-http",
+			},
+			Port: &v12.RoutePort{
+				TargetPort: intstr.FromString("keycloak"),
+			},
+			TLS: &v12.TLSConfig{
+				Termination: v12.TLSTerminationReencrypt,
+			},
+			WildcardPolicy: v12.WildcardPolicyNone,
 		}
-		edgeRoute.Spec.Port = &v12.RoutePort{
-			TargetPort: intstr.FromString("keycloak"),
-		}
-		edgeRoute.Spec.TLS = &v12.TLSConfig{
-			Termination: v12.TLSTerminationReencrypt,
-		}
-		edgeRoute.Spec.WildcardPolicy = v12.WildcardPolicyNone
 		return nil
 	})
 
