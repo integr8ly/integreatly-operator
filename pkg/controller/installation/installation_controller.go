5d09cea14e1f83cc34aa08b021c8991bbd2f0968
Gerard Ryan <gerard@ryan.lt>
2020-01-22 22:49:54 +0000

    ✨ Watch all Secrets & trigger reconcile of all CRs

--- pkg/controller/installation/installation_controller.go
+++ pkg/controller/installation/installation_controller.go
@@ -94,6 +94,25 @@ func add(mgr manager.Manager, r ReconcileInstallation) error {
 		return err
 	}
 
+	// Watch for changes to Secrets to trigger reconciliation if the SMTP Secret changes.
+	//
+	// We don't know what the Secret name is at this point, so there's a predicate here to
+	// trigger only based on appropriately labelled Secrets in the namespace.
+	//
+	// Additionally, we don't know what the Installation CR name is, so if this watch picks up
+	// an event, it will enqueue a reconcile request for _all_ Installation CRs (in the same
+	// namespace as the Secret).
+	enqueueAllInstallations := &handler.EnqueueRequestsFromMapFunc{
+		ToRequests: installationMapper{context: context.TODO(), client: mgr.GetClient()},
+	}
+	err = c.Watch(
+		&source.Kind{Type: &corev1.Secret{}},
+		enqueueAllInstallations,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -129,6 +148,7 @@ func createInstallationCR(ctx context.Context, serverClient k8sclient.Client) er
 				Type:            string(integreatlyv1alpha1.InstallationTypeManaged),
 				NamespacePrefix: DefaultInstallationPrefix,
 				SelfSignedCerts: false,
+				SMTPSecret:      "rhmi-smtp",
 			},
 		}
 
@@ -483,7 +503,7 @@ func (r *ReconcileInstallation) addCustomInformer(crd runtime.Object, namespace 
 	}
 	cache, err := cache.New(r.restConfig, cache.Options{Namespace: namespace, Scheme: r.mgr.GetScheme(), Mapper: mapper})
 	if err != nil {
-		return fmt.Errorf("Failed to create infromer cachein %s namespace: %v", namespace, err)
+		return fmt.Errorf("Failed to create informer cache in %s namespace: %v", namespace, err)
 	}
 	informer, err := cache.GetInformerForKind(crd.GetObjectKind().GroupVersionKind())
 	if err != nil {
---
+++ pkg/controller/installation/watch.go
@@ -0,0 +1,45 @@
+package installation
+
+import (
+	"context"
+
+	integreatlyv1alpha1 "github.com/integr8ly/integreatly-operator/pkg/apis/integreatly/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/types"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// installationManager implements the Mapper interface so that it can be passed to
+// handler.EnqueueRequestsFromMapFunc{}.
+//
+// The purpose of it is to be able to enqueue reconcile requests for ALL Installation CRs in a
+// namespace, when a watch picks up a relevant event.
+type installationMapper struct {
+	context context.Context
+	client  k8sclient.Client
+}
+
+func (m installationMapper) Map(mo handler.MapObject) []reconcile.Request {
+	installationList := &integreatlyv1alpha1.InstallationList{}
+	listOpts := []k8sclient.ListOption{
+		k8sclient.InNamespace(mo.Meta.GetNamespace()),
+	}
+	err := m.client.List(m.context, installationList, listOpts...)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+
+	requests := make([]reconcile.Request, len(installationList.Items))
+	for _, installation := range installationList.Items {
+		requests = append(requests, reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      installation.Name,
+				Namespace: installation.Namespace,
+			},
+		})
+	}
+
+	return requests
+}
