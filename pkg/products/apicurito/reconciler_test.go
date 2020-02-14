1d058d221bfc5c55001560ee94531e1ca1ea1d84
Brian Gallagher <briangal@gmail.com>
2020-02-14 11:06:02 +0000

    add unit tests

--- pkg/products/apicurito/reconciler_test.go
+++ pkg/products/apicurito/reconciler_test.go
@@ -1,7 +1,283 @@
 package apicurito
 
-import "testing"
+import (
+	"context"
+	apicurito "github.com/integr8ly/integreatly-operator/pkg/apis/apicur/v1alpha1"
+	integreatlyv1alpha1 "github.com/integr8ly/integreatly-operator/pkg/apis/integreatly/v1alpha1"
+	monitoring "github.com/integr8ly/integreatly-operator/pkg/apis/monitoring/v1alpha1"
+	moqclient "github.com/integr8ly/integreatly-operator/pkg/client"
+	"github.com/integr8ly/integreatly-operator/pkg/config"
+	"github.com/integr8ly/integreatly-operator/pkg/resources"
+	"github.com/integr8ly/integreatly-operator/pkg/resources/marketplace"
+	keycloak "github.com/keycloak/keycloak-operator/pkg/apis/keycloak/v1alpha1"
+	projectv1 "github.com/openshift/api/project/v1"
+	routev1 "github.com/openshift/api/route/v1"
+	usersv1 "github.com/openshift/api/user/v1"
+	coreosv1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1"
+	operatorsv1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
+	"github.com/operator-framework/operator-lifecycle-manager/pkg/lib/ownerutil"
+	marketplacev1 "github.com/operator-framework/operator-marketplace/pkg/apis/operators/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"testing"
+)
+
+var (
+	defaultOperandNamespace = "apicurito"
+)
 
 func TestReconciler_fullReconcile(t *testing.T) {
 
+	scheme, err := getBuildScheme()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	installation := &integreatlyv1alpha1.RHMI{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:       "installation",
+			Namespace:  defaultOperandNamespace,
+			Finalizers: []string{"finalizer.apicurito.integreatly.org"},
+			UID:        types.UID("xyz"),
+		},
+		TypeMeta: metav1.TypeMeta{
+			Kind:       integreatlyv1alpha1.SchemaGroupVersionKind.Kind,
+			APIVersion: integreatlyv1alpha1.SchemeGroupVersion.String(),
+		},
+		Status: integreatlyv1alpha1.RHMIStatus{
+			Stages: map[integreatlyv1alpha1.StageName]*integreatlyv1alpha1.RHMIStageStatus{
+				"apicurito-stage": {
+					Name: "apicurito-stage",
+					Products: map[integreatlyv1alpha1.ProductName]*integreatlyv1alpha1.RHMIProductStatus{
+						integreatlyv1alpha1.ProductApicurito: {
+							Name:   integreatlyv1alpha1.ProductApicurito,
+							Status: integreatlyv1alpha1.PhaseCreatingComponents,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: defaultOperandNamespace,
+			Labels: map[string]string{
+				resources.OwnerLabelKey: string(installation.GetUID()),
+			},
+		},
+		Status: corev1.NamespaceStatus{
+			Phase: corev1.NamespaceActive,
+		},
+	}
+
+	operatorNS := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: defaultOperandNamespace + "-operator",
+			Labels: map[string]string{
+				resources.OwnerLabelKey: string(installation.GetUID()),
+			},
+		},
+		Status: corev1.NamespaceStatus{
+			Phase: corev1.NamespaceActive,
+		},
+	}
+
+	cases := []struct {
+		Name           string
+		ExpectError    bool
+		ExpectedStatus integreatlyv1alpha1.StatusPhase
+		ExpectedError  string
+		FakeConfig     *config.ConfigReadWriterMock
+		FakeClient     k8sclient.Client
+		FakeMPM        *marketplace.MarketplaceInterfaceMock
+		Installation   *integreatlyv1alpha1.RHMI
+		Product        *integreatlyv1alpha1.RHMIProductStatus
+		Recorder       record.EventRecorder
+	}{
+		{
+			Name:           "test successful reconcile",
+			ExpectedStatus: integreatlyv1alpha1.PhaseCompleted,
+			FakeClient:     moqclient.NewSigsClientMoqWithScheme(scheme, installation, getApicuritoCr(), ns, operatorNS, getSecret(), getRoute()),
+			FakeConfig:     basicConfigMock(),
+			FakeMPM: &marketplace.MarketplaceInterfaceMock{
+				InstallOperatorFunc: func(ctx context.Context, serverClient k8sclient.Client, owner ownerutil.Owner, t marketplace.Target, operatorGroupNamespaces []string, approvalStrategy operatorsv1alpha1.Approval) error {
+					return nil
+				},
+				GetSubscriptionInstallPlansFunc: func(ctx context.Context, serverClient k8sclient.Client, subName string, ns string) (plans *operatorsv1alpha1.InstallPlanList, subscription *operatorsv1alpha1.Subscription, e error) {
+					return &operatorsv1alpha1.InstallPlanList{
+							Items: []operatorsv1alpha1.InstallPlan{
+								{
+									ObjectMeta: metav1.ObjectMeta{
+										Name: "apicurito-install-plan",
+									},
+									Status: operatorsv1alpha1.InstallPlanStatus{
+										Phase: operatorsv1alpha1.InstallPlanPhaseComplete,
+									},
+								},
+							},
+						}, &operatorsv1alpha1.Subscription{
+							Status: operatorsv1alpha1.SubscriptionStatus{
+								Install: &operatorsv1alpha1.InstallPlanReference{
+									Name: "apicurito-install-plan",
+								},
+							},
+						}, nil
+				},
+			},
+			Installation: installation,
+			Product:      &integreatlyv1alpha1.RHMIProductStatus{},
+			Recorder:     setupRecorder(),
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			testReconciler, err := NewReconciler(
+				tc.FakeConfig,
+				tc.Installation,
+				tc.FakeMPM,
+				tc.Recorder,
+			)
+			if err != nil && err.Error() != tc.ExpectedError {
+				t.Fatalf("unexpected error : '%v', expected: '%v'", err, tc.ExpectedError)
+			}
+
+			status, err := testReconciler.Reconcile(context.TODO(), tc.Installation, tc.Product, tc.FakeClient)
+
+			if err != nil && !tc.ExpectError {
+				t.Fatalf("expected no errors, but got one: %v", err)
+			}
+
+			if err == nil && tc.ExpectError {
+				t.Fatal("expected error but got none")
+			}
+
+			if status != tc.ExpectedStatus {
+				t.Fatalf("Expected status: '%v', got: '%v'", tc.ExpectedStatus, status)
+			}
+		})
+	}
+
+}
+
+func getSecret() *corev1.Secret {
+	return &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "samples-registry-credentials",
+			Namespace: "openshift",
+		},
+		Data: map[string][]byte{
+			"credentialKeyID":     []byte("test"),
+			"credentialSecretKey": []byte("test"),
+		},
+	}
+}
+
+func getRoute() *routev1.Route {
+	return &routev1.Route{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "apicurito",
+			Namespace: defaultOperandNamespace,
+		},
+		Spec: routev1.RouteSpec{
+			Host: "sampleHost",
+		},
+	}
+}
+
+func getApicuritoCr() *apicurito.Apicurito {
+
+	apicuritoCR := &apicurito.Apicurito{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      apicuritoName,
+			Namespace: defaultOperandNamespace,
+		},
+	}
+	return apicuritoCR
+}
+
+func basicConfigMock() *config.ConfigReadWriterMock {
+	return &config.ConfigReadWriterMock{
+		GetOperatorNamespaceFunc: func() string {
+			return defaultOperandNamespace + "-operator"
+		},
+		ReadApicuritoFunc: func() (apicurito *config.Apicurito, err error) {
+			return config.NewApicurito(config.ProductConfig{
+				"NAMESPACE": defaultOperandNamespace,
+			}), nil
+		},
+		WriteConfigFunc: func(config config.ConfigReadable) error {
+			return nil
+		},
+	}
+}
+
+func setupRecorder() record.EventRecorder {
+	return record.NewFakeRecorder(50)
+}
+
+func getBuildScheme() (*runtime.Scheme, error) {
+	scheme := runtime.NewScheme()
+	//err := threescalev1.SchemeBuilder.AddToScheme(scheme)
+	//if err != nil {
+	//	return nil, err
+	//}
+	err := keycloak.SchemeBuilder.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	err = integreatlyv1alpha1.SchemeBuilder.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	err = operatorsv1alpha1.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	err = apicurito.SchemeBuilder.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	err = marketplacev1.SchemeBuilder.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	err = corev1.SchemeBuilder.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	err = coreosv1.SchemeBuilder.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	//err = kafkav1.SchemeBuilder.AddToScheme(scheme)
+	//if err != nil {
+	//	return nil, err
+	//}
+	err = usersv1.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	//err = oauthv1.AddToScheme(scheme)
+	//if err != nil {
+	//	return nil, err
+	//}
+	err = monitoring.SchemeBuilder.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	err = routev1.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	err = projectv1.AddToScheme(scheme)
+	if err != nil {
+		return nil, err
+	}
+	return scheme, err
 }
