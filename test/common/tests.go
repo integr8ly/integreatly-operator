0e7ac66f6e636dd5cc7a9f796f4ce07d7d613174
dimitraz <dzuccare@redhat.com>
2020-03-02 15:24:33 +0000

    change test order

--- test/common/tests.go
+++ test/common/tests.go
@@ -5,11 +5,11 @@ var (
 		// Add all the tests that should be executed in both e2e and osd suites here.
 		// It is an array so the tests will be executed in the same order as they defined here.
 		{"Verify CRD Exists", TestIntegreatlyCRDExists},
-		{"Verify alerts exist", TestIntegreatlyAlertsExist},
 	}
 
 	AFTER_INSTALL_TESTS = []TestCase{
 		{"Verify Deployment resources have the expected replicas", TestDeploymentExpectedReplicas},
 		{"Verify Deployment Config resources have the expected replicas", TestDeploymentConfigExpectedReplicas},
+		{"Verify alerts exist", TestIntegreatlyAlertsExist},
 	}
 )
