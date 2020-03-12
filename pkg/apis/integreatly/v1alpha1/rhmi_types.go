5e5fae0d21b0dbfa7760bdb271c0467150569a5e
Peter Braun <pbraun@redhat.com>
2020-03-12 17:59:57 +0100

    update webapp to 2.22.1

--- pkg/apis/integreatly/v1alpha1/rhmi_types.go
+++ pkg/apis/integreatly/v1alpha1/rhmi_types.go
@@ -74,7 +74,7 @@ var (
 	OperatorVersionAMQStreams          OperatorVersion = "1.1.0"
 	OperatorVersionAMQOnline           OperatorVersion = "1.3.1"
 	OperatorVersionMonitoring          OperatorVersion = "1.1.4"
-	OperatorVersionSolutionExplorer    OperatorVersion = "0.0.45"
+	OperatorVersionSolutionExplorer    OperatorVersion = "0.0.46"
 	OperatorVersionRHSSO               OperatorVersion = "8.0.1"
 	OperatorVersionRHSSOUser           OperatorVersion = "8.0.1"
 	OperatorVersionCodeReadyWorkspaces OperatorVersion = "2.0.0"
