1d76b9814dd323dcaf04e93a8146866b0acdda4a
Davide <david3bizzarri@gmail.com>
2020-03-13 13:57:38 +0100

    fix and improve expected replicas tests
    add stateful set test to tests
    move d, dc, st to global variables
    fail without braking on the first error
    verify that the number of replicas are also available
    use InstallationName constant for postgres deployments

--- test/common/deployment_types.go
+++ test/common/deployment_types.go
@@ -20,160 +20,8 @@ type Product struct {
 	ExpectedReplicas int32
 }
 
-func TestDeploymentExpectedReplicas(t *testing.T, ctx *TestingContext) {
-	deployment := getDeployment()
-
-	isClusterStorage, err := isClusterStorage(ctx)
-	if err != nil {
-		t.Fatal("error getting isClusterStorage:", err)
-	}
-	// If the cluster is using in cluster storage instead of AWS resources
-	// These deployments will also need to be checked
-	if isClusterStorage {
-		clusterStorageDeployments := getClusterStorageDeployments()
-		for _, clusterStorageDeployment := range clusterStorageDeployments {
-			deployment = append(deployment, clusterStorageDeployment)
-		}
-	}
-
-	for _, namespace := range deployment {
-		for _, product := range namespace.Products {
-			deployment, err := ctx.KubeClient.AppsV1().Deployments(namespace.Name).Get(product.Name, v1.GetOptions{})
-			if err != nil {
-				t.Fatalf("Deployment %s not found - %s", product.Name, err)
-			}
-			if deployment.Status.Replicas != product.ExpectedReplicas {
-				t.Fatalf("Deployment %s in namespace %s doens't match the number of expected replicas %s/%s",
-					product.Name,
-					namespace.Name,
-					string(deployment.Status.Replicas),
-					string(product.ExpectedReplicas),
-				)
-			}
-		}
-	}
-}
-
-func TestDeploymentConfigExpectedReplicas(t *testing.T, ctx *TestingContext) {
-
-	deploymentConfigNS := getDeploymentConfig()
-	for _, namespace := range deploymentConfigNS {
-		for _, product := range namespace.Products {
-
-			dc := &appsv1.DeploymentConfig{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      product.Name,
-					Namespace: namespace.Name,
-				},
-			}
-			err := ctx.Client.Get(goctx.TODO(), k8sclient.ObjectKey{Name: product.Name, Namespace: namespace.Name}, dc)
-			if err != nil {
-				t.Fatalf("DeploymentConfig %s not found - %s", product.Name, err)
-			}
-
-			if dc.Status.Replicas != product.ExpectedReplicas {
-				t.Fatalf("DeploymentConfig %s in namespace %s doens't match the number of expected replicas %s/%s",
-					product.Name,
-					namespace.Name,
-					string(dc.Status.Replicas),
-					string(product.ExpectedReplicas),
-				)
-			}
-
-		}
-	}
-}
-
-func TestStatefulSetsExpectedReplicas(t *testing.T, ctx *TestingContext) {
-
-	statefulSetsNS := getStatefulSets()
-	for _, namespace := range statefulSetsNS {
-		for _, product := range namespace.Products {
-
-			statefulSet, err := ctx.KubeClient.AppsV1().StatefulSets(namespace.Name).Get(product.Name, v1.GetOptions{})
-			if err != nil {
-				t.Fatalf("StatefulSet %s not found - %s", product.Name, err)
-			}
-
-			if statefulSet.Status.Replicas != product.ExpectedReplicas {
-				t.Fatalf("StatefulSet %s in namespace %s doens't match the number of expected replicas %s/%s",
-					product.Name,
-					namespace.Name,
-					string(statefulSet.Status.Replicas),
-					string(product.ExpectedReplicas),
-				)
-			}
-
-		}
-	}
-}
-
-func getStatefulSets() []Namespace {
-	return []Namespace{
-		{
-			Name: "redhat-rhmi-middleware-monitoring-operator",
-			Products: []Product{
-				Product{Name: "alertmanager-application-monitoring", ExpectedReplicas: 1},
-				Product{Name: "prometheus-application-monitoring", ExpectedReplicas: 1},
-			},
-		},
-		{
-			Name: "redhat-rhmi-rhsso",
-			Products: []Product{
-				Product{Name: "keycloak", ExpectedReplicas: 2},
-			},
-		},
-		{
-			Name: "redhat-rhmi-user-sso",
-			Products: []Product{
-				Product{Name: "keycloak", ExpectedReplicas: 2},
-			},
-		},
-	}
-}
-
-func getDeploymentConfig() []Namespace {
-	return []Namespace{
-		{
-			Name: "redhat-rhmi-3scale",
-			Products: []Product{
-				Product{Name: "apicast-production", ExpectedReplicas: 2},
-				Product{Name: "apicast-staging", ExpectedReplicas: 2},
-				Product{Name: "backend-cron", ExpectedReplicas: 2},
-				Product{Name: "backend-listener", ExpectedReplicas: 2},
-				Product{Name: "backend-worker", ExpectedReplicas: 2},
-				Product{Name: "system-app", ExpectedReplicas: 2},
-				Product{Name: "system-memcache", ExpectedReplicas: 1},
-				Product{Name: "system-sidekiq", ExpectedReplicas: 2},
-				Product{Name: "system-sphinx", ExpectedReplicas: 1},
-				Product{Name: "zync", ExpectedReplicas: 2},
-				Product{Name: "zync-database", ExpectedReplicas: 1},
-				Product{Name: "zync-que", ExpectedReplicas: 2},
-			},
-		},
-		{
-			Name: "redhat-rhmi-fuse",
-			Products: []Product{
-				Product{Name: "broker-amq", ExpectedReplicas: 0},
-				Product{Name: "syndesis-db", ExpectedReplicas: 1},
-				Product{Name: "syndesis-meta", ExpectedReplicas: 1},
-				Product{Name: "syndesis-oauthproxy", ExpectedReplicas: 1},
-				Product{Name: "syndesis-prometheus", ExpectedReplicas: 1},
-				Product{Name: "syndesis-server", ExpectedReplicas: 1},
-				Product{Name: "syndesis-ui", ExpectedReplicas: 1},
-			},
-		},
-		{
-			Name: "redhat-rhmi-solution-explorer",
-			Products: []Product{
-				Product{Name: "tutorial-web-app", ExpectedReplicas: 1},
-			},
-		},
-	}
-}
-
-func getDeployment() []Namespace {
-	return []Namespace{
+var (
+	deployments = []Namespace{
 		{
 			Name: "redhat-rhmi-3scale-operator",
 			Products: []Product{
@@ -264,22 +112,202 @@ func getDeployment() []Namespace {
 			},
 		},
 	}
-}
-
-func getClusterStorageDeployments() []Namespace {
-	return []Namespace{
+	clusterStorageDeployments = []Namespace{
 		{
 			Name: "redhat-rhmi-operator",
 			Products: []Product{
-				Product{Name: "codeready-postgres-rhmi", ExpectedReplicas: 1},
-				Product{Name: "threescale-backend-redis-rhmi", ExpectedReplicas: 1},
-				Product{Name: "threescale-postgres-rhmi", ExpectedReplicas: 1},
-				Product{Name: "threescale-redis-rhmi", ExpectedReplicas: 1},
-				Product{Name: "ups-postgres-rhmi", ExpectedReplicas: 1},
-				Product{Name: "rhsso-postgres-rhmi", ExpectedReplicas: 1},
-				Product{Name: "rhssouser-postgres-rhmi", ExpectedReplicas: 1},
+				Product{Name: "codeready-postgres-" + InstallationName, ExpectedReplicas: 1},
+				Product{Name: "threescale-backend-redis-" + InstallationName, ExpectedReplicas: 1},
+				Product{Name: "threescale-postgres-" + InstallationName, ExpectedReplicas: 1},
+				Product{Name: "threescale-redis-" + InstallationName, ExpectedReplicas: 1},
+				Product{Name: "ups-postgres-" + InstallationName, ExpectedReplicas: 1},
+				Product{Name: "rhsso-postgres-" + InstallationName, ExpectedReplicas: 1},
+				Product{Name: "rhssouser-postgres-" + InstallationName, ExpectedReplicas: 1},
 				Product{Name: "standard-authservice-postgresql", ExpectedReplicas: 1},
 			},
 		},
+	}
+	deploymentConfigs = []Namespace{
+		{
+			Name: "redhat-rhmi-3scale",
+			Products: []Product{
+				Product{Name: "apicast-production", ExpectedReplicas: 2},
+				Product{Name: "apicast-staging", ExpectedReplicas: 2},
+				Product{Name: "backend-cron", ExpectedReplicas: 2},
+				Product{Name: "backend-listener", ExpectedReplicas: 2},
+				Product{Name: "backend-worker", ExpectedReplicas: 2},
+				Product{Name: "system-app", ExpectedReplicas: 2},
+				Product{Name: "system-memcache", ExpectedReplicas: 1},
+				Product{Name: "system-sidekiq", ExpectedReplicas: 2},
+				Product{Name: "system-sphinx", ExpectedReplicas: 1},
+				Product{Name: "zync", ExpectedReplicas: 2},
+				Product{Name: "zync-database", ExpectedReplicas: 1},
+				Product{Name: "zync-que", ExpectedReplicas: 2},
+			},
+		},
+		{
+			Name: "redhat-rhmi-fuse",
+			Products: []Product{
+				Product{Name: "broker-amq", ExpectedReplicas: 0},
+				Product{Name: "syndesis-db", ExpectedReplicas: 1},
+				Product{Name: "syndesis-meta", ExpectedReplicas: 1},
+				Product{Name: "syndesis-oauthproxy", ExpectedReplicas: 1},
+				Product{Name: "syndesis-prometheus", ExpectedReplicas: 1},
+				Product{Name: "syndesis-server", ExpectedReplicas: 1},
+				Product{Name: "syndesis-ui", ExpectedReplicas: 1},
+			},
+		},
+		{
+			Name: "redhat-rhmi-solution-explorer",
+			Products: []Product{
+				Product{Name: "tutorial-web-app", ExpectedReplicas: 1},
+			},
+		},
+	}
+	statefulSets = []Namespace{
+		{
+			Name: "redhat-rhmi-middleware-monitoring-operator",
+			Products: []Product{
+				Product{Name: "alertmanager-application-monitoring", ExpectedReplicas: 1},
+				Product{Name: "prometheus-application-monitoring", ExpectedReplicas: 1},
+			},
+		},
+		{
+			Name: "redhat-rhmi-rhsso",
+			Products: []Product{
+				Product{Name: "keycloak", ExpectedReplicas: 2},
+			},
+		},
+		{
+			Name: "redhat-rhmi-user-sso",
+			Products: []Product{
+				Product{Name: "keycloak", ExpectedReplicas: 2},
+			},
+		},
+	}
+)
+
+func TestDeploymentExpectedReplicas(t *testing.T, ctx *TestingContext) {
+	deployments := deployments
+
+	isClusterStorage, err := isClusterStorage(ctx)
+	if err != nil {
+		t.Fatal("error getting isClusterStorage:", err)
+	}
+
+	// If the cluster is using in cluster storage instead of AWS resources
+	// These deployments will also need to be checked
+	if isClusterStorage {
+		for _, d := range clusterStorageDeployments {
+			deployments = append(deployments, d)
+		}
+	}
+
+	for _, namespace := range deployments {
+		for _, product := range namespace.Products {
+
+			deployment, err := ctx.KubeClient.AppsV1().Deployments(namespace.Name).Get(product.Name, v1.GetOptions{})
+			if err != nil {
+				// Fail the test without failing immideatlly
+				t.Errorf("Failed to get Deployment %s in namespace %s with error: %s", product.Name, namespace.Name, err)
+				continue
+			}
+
+			if deployment.Status.Replicas != product.ExpectedReplicas {
+				t.Errorf("Deployment %s in namespace %s doens't match the number of expected replicas %s/%s",
+					product.Name,
+					namespace.Name,
+					string(deployment.Status.Replicas),
+					string(product.ExpectedReplicas),
+				)
+				continue
+			}
+
+			// Verify that the expected replicas are also available, means they are up and running and consumable by users
+			if deployment.Status.AvailableReplicas != product.ExpectedReplicas {
+				t.Errorf("Deployment %s in namespace %s doens't match the number of expected available replicas %s/%s",
+					product.Name,
+					namespace.Name,
+					string(deployment.Status.AvailableReplicas),
+					string(product.ExpectedReplicas),
+				)
+				continue
+			}
+		}
+	}
+}
+
+func TestDeploymentConfigExpectedReplicas(t *testing.T, ctx *TestingContext) {
+
+	for _, namespace := range deploymentConfigs {
+		for _, product := range namespace.Products {
+
+			deploymentConfig := &appsv1.DeploymentConfig{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      product.Name,
+					Namespace: namespace.Name,
+				},
+			}
+			err := ctx.Client.Get(goctx.TODO(), k8sclient.ObjectKey{Name: product.Name, Namespace: namespace.Name}, deploymentConfig)
+			if err != nil {
+				t.Errorf("Failed to get DeploymentConfig %s in namespace %s with error: %s", product.Name, namespace.Name, err)
+				continue
+			}
+
+			if deploymentConfig.Status.Replicas != product.ExpectedReplicas {
+				t.Errorf("DeploymentConfig %s in namespace %s doens't match the number of expected replicas %s/%s",
+					product.Name,
+					namespace.Name,
+					string(deploymentConfig.Status.Replicas),
+					string(product.ExpectedReplicas),
+				)
+				continue
+			}
+
+			if deploymentConfig.Status.AvailableReplicas != product.ExpectedReplicas {
+				t.Errorf("DeploymentConfig %s in namespace %s doens't match the number of expected available replicas %s/%s",
+					product.Name,
+					namespace.Name,
+					string(deploymentConfig.Status.AvailableReplicas),
+					string(product.ExpectedReplicas),
+				)
+				continue
+			}
+		}
+	}
+}
+
+func TestStatefulSetsExpectedReplicas(t *testing.T, ctx *TestingContext) {
+
+	for _, namespace := range statefulSets {
+		for _, product := range namespace.Products {
+
+			statefulSet, err := ctx.KubeClient.AppsV1().StatefulSets(namespace.Name).Get(product.Name, v1.GetOptions{})
+			if err != nil {
+				t.Errorf("Failed to get StatefulSet %s in namespace %s with error: %s", product.Name, namespace.Name, err)
+				continue
+			}
+
+			if statefulSet.Status.Replicas != product.ExpectedReplicas {
+				t.Errorf("StatefulSet %s in namespace %s doens't match the number of expected replicas %s/%s",
+					product.Name,
+					namespace.Name,
+					string(statefulSet.Status.Replicas),
+					string(product.ExpectedReplicas),
+				)
+				continue
+			}
+
+			// Verify the number of ReadyReplicas because the SatefulSet doesn't have the concept of AvailableReplicas
+			if statefulSet.Status.ReadyReplicas != product.ExpectedReplicas {
+				t.Errorf("StatefulSet %s in namespace %s doens't match the number of expected ready replicas %s/%s",
+					product.Name,
+					namespace.Name,
+					string(statefulSet.Status.ReadyReplicas),
+					string(product.ExpectedReplicas),
+				)
+				continue
+			}
+		}
 	}
 }
