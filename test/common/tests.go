761b2b0b8eb9961b7ce2bd96f739bfa269697bb9
Davide <david3bizzarri@gmail.com>
2020-04-03 11:41:54 +0200

    describe where the ids are coming from

--- test/common/tests.go
+++ test/common/tests.go
@@ -1,5 +1,8 @@
 package common
 
+// All tests are be linked[1] to the integreatly-test-cases[2] repo by using the same ID
+// 1. https://gitlab.cee.redhat.com/integreatly-qe/integreatly-test-cases#how-to-automate-a-test-case-and-link-it-back
+// 2. https://gitlab.cee.redhat.com/integreatly-qe/integreatly-test-cases
 var (
 	ALL_TESTS = []TestCase{
 		// Add all the tests that should be executed in both e2e and osd suites here.
