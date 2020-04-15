896dce930e91609ac2a25d5ca6abbb9696176c68
Oleg Matskiv <omatskiv@redhat.com>
2020-04-15 17:53:19 +0300

    Revert "Fixing login flow issue in E2E tests"
    This reverts commit f0658de53641c1ea5673a656d2e3e6e51101a726.

--- test/resources/openshift_auth.go
+++ test/resources/openshift_auth.go
@@ -150,8 +150,7 @@ func openshiftClientSetup(url, username, password string, client *http.Client, i
 	if strings.Contains(browser.Url().Host, openshiftOauthSubdomain) {
 		permissionsForm, err := browser.Form("[action=approve]")
 		if err != nil {
-			// Permissions were already approved
-			return nil
+			return fmt.Errorf("failed to get permissions form: %w", err)
 		}
 		if err = permissionsForm.Submit(); err != nil {
 			return fmt.Errorf("failed to submit acceptance button for permissions: %w", err)
