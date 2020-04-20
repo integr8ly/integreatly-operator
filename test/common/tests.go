417c075237927d970171807bbf55e1e6d62ad44f
Davide <david3bizzarri@gmail.com>
2020-04-20 12:07:35 +0200

    Add AMQ Streams replicas test

--- test/common/tests.go
+++ test/common/tests.go
@@ -31,5 +31,6 @@ var (
 		{"B06 - Verify users with no email get default email", TestDefaultUserEmail},
 		{"F05 - Verify Replicas Scale correctly in Threescale", TestReplicasInThreescale},
 		{"F06 - Verify Replicas Scale correctly in Apicurito", TestReplicasInApicurito},
+		{"F07 - Verify Replicas Scale correctly in AMQ Streams", TestReplicasInAmqstreams},
 	}
 )
