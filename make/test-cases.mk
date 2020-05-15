73886feee7e85d91ec1e99a24e3e58c940a9948d
Davide <david3bizzarri@gmail.com>
2020-05-15 15:50:42 +0200

    add make cmd to fix test-cases

--- make/test-cases.mk
+++ make/test-cases.mk
@@ -2,4 +2,11 @@
 .PHONY: test-cases/lint
 test-cases/lint:
 	@cd test-cases/ && npm install
-	@cd test-cases/ && npm run lint
+	@cd test-cases/ && npm run lint || \
+		echo -e "\nRun 'make test-cases/fix' to automatically fix all lint issues\n"
+
+.PHONY: test-cases/fix
+test-cases/fix:
+	@cd test-cases/ && npm install
+	@cd test-cases/ && npm run prettier
+	@cd test-cases/ && npm run rename
