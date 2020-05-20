a964d398838cb8798a1b6d9c90885341ec588a51
Cathal O'Connor <catoconn@redhat.com>
2020-05-20 08:42:59 +0100

    INTLY-7238 Switched from update to patch
    Switched from update to patch and check for readyreplicas before passing test.

--- test/common/scale_up_replicas_apicurito.go
+++ test/common/scale_up_replicas_apicurito.go
@@ -7,8 +7,8 @@ import (
 	"time"
 
 	apicuritov1alpha1 "github.com/apicurio/apicurio-operators/apicurito/pkg/apis/apicur/v1alpha1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -22,6 +22,8 @@ const (
 	apicuritoNamespace         = "redhat-rhmi-apicurito"
 	retryIntervalApicurito     = time.Second * 20
 	timeoutApicurito           = time.Minute * 7
+	requestURLApicturito       = "/apis/apicur.io/v1alpha1"
+	kindApicurito              = "apicuritos"
 )
 
 func TestReplicasInApicurito(t *testing.T, ctx *TestingContext) {
@@ -36,7 +38,7 @@ func TestReplicasInApicurito(t *testing.T, ctx *TestingContext) {
 		t.Fatalf("Incorrect number of replicas to start : %v", err)
 	}
 
-	apicuritoCR, err = updateApicurito(ctx.Client, scaleUpApicuritoReplicas, t)
+	apicuritoCR, err = updateApicurito(ctx, scaleUpApicuritoReplicas, t)
 	if err != nil {
 		t.Fatalf("Unable to update : %v", err)
 	}
@@ -46,7 +48,7 @@ func TestReplicasInApicurito(t *testing.T, ctx *TestingContext) {
 		t.Fatalf("Incorrect number of replicas : %v", err)
 	}
 
-	apicuritoCR, err = updateApicurito(ctx.Client, scaleDownApicuritoReplicas, t)
+	apicuritoCR, err = updateApicurito(ctx, scaleDownApicuritoReplicas, t)
 	if err != nil {
 		t.Fatalf("Unable to update : %v", err)
 	}
@@ -56,6 +58,31 @@ func TestReplicasInApicurito(t *testing.T, ctx *TestingContext) {
 		t.Fatalf("Incorrect number of replicas : %v", err)
 	}
 
+	t.Log("Checking replicas are ready")
+	if err := checkReplicasAreReady(ctx, t, numberOfApicuritoReplicas); err != nil {
+		t.Fatalf("Replicas weren't ready within timeout")
+	}
+
+}
+
+func checkReplicasAreReady(dynClient *TestingContext, t *testing.T, replicas int32) error {
+
+	err := wait.Poll(retryInterval, timeout, func() (done bool, err error) {
+
+		deployment, err := dynClient.KubeClient.AppsV1().Deployments(apicuritoNamespace).Get(apicuritoName, metav1.GetOptions{})
+		if err != nil {
+			t.Errorf("Failed to get Deployment %s in namespace %s with error: %s", apicuritoName, apicuritoNamespace, err)
+		}
+		if deployment.Status.ReadyReplicas == replicas {
+			t.Logf("Replicas Ready %v", deployment.Status.ReadyReplicas)
+			return true, nil
+		}
+		return false, fmt.Errorf("%v", deployment.Status.ReadyReplicas)
+	})
+	if err != nil {
+		return fmt.Errorf("Number of replicas for apicurito.Spec.Size is not correct : Replicas - %v, Expected - %v", err, replicas)
+	}
+	return nil
 }
 
 func getApicurito(dynClient k8sclient.Client) (apicuritov1alpha1.Apicurito, error) {
@@ -68,26 +95,27 @@ func getApicurito(dynClient k8sclient.Client) (apicuritov1alpha1.Apicurito, erro
 	return *apicuritoCR, nil
 }
 
-func updateApicurito(dynClient k8sclient.Client, replicas int32, t *testing.T) (apicuritov1alpha1.Apicurito, error) {
+func updateApicurito(ctx *TestingContext, replicas int32, t *testing.T) (apicuritov1alpha1.Apicurito, error) {
 
-	apicuritoCR, err := getApicurito(dynClient)
+	replica := fmt.Sprintf(`{
+		"apiVersion": "apicur.io/v1alpha1",
+		"kind": "Apicurito",
+		"spec": {
+			"size": %[1]v
+		}
+	}`, replicas)
 
+	replicaBytes := []byte(replica)
+
+	request := ctx.ExtensionClient.RESTClient().Patch(types.MergePatchType).
+		Resource(kindApicurito).
+		Name(apicuritoName).
+		Namespace(apicuritoNamespace).
+		RequestURI(requestURLApicturito).Body(replicaBytes).Do()
+	_, err := request.Raw()
+
+	apicuritoCR, err := getApicurito(ctx.Client)
 	if err != nil {
-		return apicuritoCR, err
-	}
-	apicuritoCR = apicuritov1alpha1.Apicurito{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:            apicuritoName,
-			Namespace:       apicuritoNamespace,
-			ResourceVersion: apicuritoCR.GetResourceVersion(),
-		},
-		Spec: apicuritov1alpha1.ApicuritoSpec{
-			Image: apicuritoCR.Spec.Image,
-			Size:  replicas,
-		},
-	}
-
-	if err := dynClient.Update(goctx.TODO(), apicuritoCR.DeepCopy(), &k8sclient.UpdateOptions{}); err != nil {
 		return apicuritoCR, err
 	}
 
