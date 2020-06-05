da1fa19e41f5b6a12adba66fe96ef7db321074d6
Davide <david3bizzarri@gmail.com>
2020-06-05 15:06:42 +0200

    fix exit error in test-cases lint

--- make/test-cases.mk
+++ make/test-cases.mk
@@ -3,8 +3,7 @@
 test-cases/lint:
 	cd test-cases/ && npm install
 	cd test-cases/ && npm run lint || \
-		echo -e "\nRun 'make test-cases/fix' to automatically fix all lint issues\n" ;
-		exit 23
+		(echo -e "\nRun 'make test-cases/fix' to automatically fix all lint issues\n"; exit 23)
 
 .PHONY: test-cases/fix
 test-cases/fix:
