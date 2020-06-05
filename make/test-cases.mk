e448e7f3427332384a04be3ba9d77c0041b2a029
Davide <david3bizzarri@gmail.com>
2020-06-05 13:48:29 +0200

    return error when test-cases/lint fails

--- make/test-cases.mk
+++ make/test-cases.mk
@@ -1,12 +1,13 @@
 
 .PHONY: test-cases/lint
 test-cases/lint:
-	@cd test-cases/ && npm install
-	@cd test-cases/ && npm run lint || \
-		echo -e "\nRun 'make test-cases/fix' to automatically fix all lint issues\n"
+	cd test-cases/ && npm install
+	cd test-cases/ && npm run lint || \
+		echo -e "\nRun 'make test-cases/fix' to automatically fix all lint issues\n" ;
+		exit 23
 
 .PHONY: test-cases/fix
 test-cases/fix:
-	@cd test-cases/ && npm install
-	@cd test-cases/ && npm run prettier
-	@cd test-cases/ && npm run rename
+	cd test-cases/ && npm install
+	cd test-cases/ && npm run prettier
+	cd test-cases/ && npm run rename
