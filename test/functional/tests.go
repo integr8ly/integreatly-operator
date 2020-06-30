57b544a94dda1f913334ac736c09eaa56c33f336
Dimitra Zuccarelli <dzuccare@redhat.com>
2020-06-30 12:36:10 +0100

    Update test description
    Co-authored-by: Pavel Sturc <psturc@redhat.com>

--- test/functional/tests.go
+++ test/functional/tests.go
@@ -7,7 +7,7 @@ var (
 		{Description: "F01 - Verify AWS rds resources exist and are in expected state", Test: AWSRDSResourcesExistTest},
 		{Description: "F03 - Verify AWS elasticache resources exist and are in expected state", Test: AWSElasticacheResourcesExistTest},
 		{Description: "A21 - Verify AWS maintenance and backup windows", Test: CROStrategyOverrideAWSResourceTest},
-		{Description: "Verify AWS Standalone VPC exists", Test: TestStandaloneVPCExists},
+		{Description: "A25 - Verify standalone RHMI VPC exists and is configured properly", Test: TestStandaloneVPCExists},
 		//TODO add F04 - Verify AWS s3 resources exist and are in expected state
 	}
 )
