01e1e9342a13d22adf821db8ca7200c98cb34dc0
Delorean <cloud-services-delorean@redhat.com>
2020-07-09 12:07:39 +0000

    prepare for release 2.5.0-rc1

--- version/version.go
+++ version/version.go
@@ -6,8 +6,8 @@ import (
 )
 
 var (
-	Version            = "2.4.0"
-	IntegreatlyVersion = "2.4.0"
+	Version            = "2.5.0"
+	IntegreatlyVersion = "2.5.0"
 )
 
 func VerifyProductAndOperatorVersion(product integreatlyv1alpha1.RHMIProductStatus, expectedProductVersion string, expectedOpVersion string) bool {
