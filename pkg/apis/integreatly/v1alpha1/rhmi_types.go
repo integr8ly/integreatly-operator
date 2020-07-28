92cd61214496b8d5430d799432f9097955c2487a
Peter Braun <pbraun@redhat.com>
2020-07-28 10:14:12 +0200

    update webapp version to 2.27.0 and SP to 1.0.12

--- pkg/apis/integreatly/v1alpha1/rhmi_types.go
+++ pkg/apis/integreatly/v1alpha1/rhmi_types.go
@@ -71,7 +71,7 @@ var (
 	VersionRHSSO               ProductVersion = "7.3"
 	VersionRHSSOUser           ProductVersion = "7.3"
 	VersionMonitoringSpec      ProductVersion = "1.0"
-	VersionSolutionExplorer    ProductVersion = "2.26.3"
+	VersionSolutionExplorer    ProductVersion = "2.27.0"
 
 	// Versioning for Fuse on OpenShift does not follow a similar pattern to other products.
 	// It is currently implicitly tied to version 7.6 of Fuse, hence the 7.6 value for VersionFuseOnOpenshift above
@@ -88,7 +88,7 @@ var (
 	OperatorVersionAMQStreams          OperatorVersion = "1.1.0"
 	OperatorVersionAMQOnline           OperatorVersion = "1.4"
 	OperatorVersionMonitoring          OperatorVersion = "1.2.1"
-	OperatorVersionSolutionExplorer    OperatorVersion = "0.0.60"
+	OperatorVersionSolutionExplorer    OperatorVersion = "0.0.61"
 	OperatorVersionRHSSO               OperatorVersion = "9.0.2"
 	OperatorVersionRHSSOUser           OperatorVersion = "9.0.2"
 	OperatorVersionCodeReadyWorkspaces OperatorVersion = "2.1.1"
