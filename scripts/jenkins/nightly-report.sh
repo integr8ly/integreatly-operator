b62080d44c0d60d97a4bca7e05a75c162b9cd49f
Delorean <cloud-services-delorean@redhat.com>
2020-07-29 08:32:55 +0000

    prepare for release 2.6.0-rc1

--- scripts/jenkins/nightly-report.sh
+++ scripts/jenkins/nightly-report.sh
@@ -31,19 +31,30 @@ getDateString() {
 }
 
 rm -f report.txt && touch report.txt
-echo -e "Nightly pipelines results for week ending at $(date +%Y-%m-%d)\n" > report.txt
+
+echo -e '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' > report.txt
+echo -e '<html xmlns="http://www.w3.org/1999/xhtml">' > report.txt
+echo -e ' <head>' > report.txt
+echo -e '  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' > report.txt
+echo -e '  <title>Nightly pipelines results for week ending at $(date +%Y-%m-%d)\n</title>' > report.txt
+echo -e '  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>' > report.txt
+echo -e '</head>' > report.txt
+
 for job in "${jobs[@]}";
 do
   echo -e "Obtaining nightly pipelines results for $job..."
   echo -e "Pipeline: $job\n" >> report.txt
   
-  firstBuildNumber=$(curl $JENKINS_URL/$job/api/json --silent | jq -r .firstBuild.number)
-  lastBuildNumber=$(curl $JENKINS_URL/$job/api/json --silent | jq -r .builds[0].number)
-  
+  firstBuildNumber=$(curl -k $JENKINS_URL/$job/api/json --silent | jq -r .firstBuild.number)
+  lastBuildNumber=$(curl -k $JENKINS_URL/$job/api/json --silent | jq -r .builds[0].number)
+
+  echo -e "Downloading stage data for $job..."
+  curl -k $JENKINS_URL/$job/wfapi/runs --silent > wfapi.json
+
   for ((buildNumber=$lastBuildNumber; buildNumber >= $firstBuildNumber; buildNumber--))
   do
     rm -f build.json
-    curl $JENKINS_URL/$job/$buildNumber/api/json --silent > build.json
+    curl -k $JENKINS_URL/$job/$buildNumber/api/json --silent > build.json
 
     errorCheck=$(cat build.json | grep "$ERROR_STRING")
 
@@ -53,7 +64,13 @@ do
     fi
 
     buildTimestamp=$(cat build.json | jq -r .timestamp)/1000
-    
+
+    cat wfapi.json | jq -c ".[] | select( .id|tonumber | contains($buildNumber)) | .stages" > stage.json
+
+    stageData=$(cat stage.json)
+
+    echo $stageData
+
     if [ $PERIOD_END_TIMESTAMP -gt $buildTimestamp ] ; then
       break
     fi
