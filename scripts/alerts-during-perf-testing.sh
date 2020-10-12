ec4409e89212f62884d17a5c010bc99a5a228fed
Austin Cunningham <aucunnin@redhat.com>
2020-10-12 14:41:24 +0100

    move function to start of script

--- scripts/alerts-during-perf-testing.sh
+++ scripts/alerts-during-perf-testing.sh
@@ -9,6 +9,17 @@
 # - oc (logged in at the cmd line in order to get the bearer token)
 PRODUCT_NAME=$1
 
+# function to check if there are no alerts firing bar deadmansnitch and remove the temp files
+function CHECK_NO_ALERTS(){
+    if [[ $(wc -l  tmp-alert-firing-during-perf-testing-report.csv) = 1  && $(wc -l  tmp-alert-pending-during-perf-testing-report.csv) = 1 ]] ; then
+      echo Only alert firing is DeadMansSwitch
+      date
+    fi
+    rm tmp-alert-firing-during-perf-testing-report.csv
+    rm tmp-alert-pending-during-perf-testing-report.csv
+  done
+}
+
 # If no args are passed in then run for all products
 if (( $# == 0 )); then
   while :; do
@@ -58,13 +69,3 @@ else
   done
 fi
 
-# function to check if there are no alerts firing bar deadmansnitch and remove the temp files
-function CHECK_NO_ALERTS(){
-    if [[ $(wc -l  tmp-alert-firing-during-perf-testing-report.csv) = 1  && $(wc -l  tmp-alert-pending-during-perf-testing-report.csv) = 1 ]] ; then
-      echo Only alert firing is DeadMansSwitch
-      date
-    fi
-    rm tmp-alert-firing-during-perf-testing-report.csv
-    rm tmp-alert-pending-during-perf-testing-report.csv
-  done
-}
