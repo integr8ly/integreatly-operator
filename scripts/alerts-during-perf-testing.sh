5dc27afa80d36e612222b5131de049fe5b9e351d
Austin Cunningham <aucunnin@redhat.com>
2020-10-14 11:46:57 +0100

    add regex to search for alerts created by keycloak and threescale operators

--- scripts/alerts-during-perf-testing.sh
+++ scripts/alerts-during-perf-testing.sh
@@ -8,6 +8,9 @@
 # - jq
 # - oc (logged in at the cmd line in order to get the bearer token)
 # VARIABLES
+RHSSO="rhsso"
+USER_SSO="user-sso"
+THREESCALE="3scale"
 TOKEN=$(oc whoami --show-token)
 MONITORING_ROUTE=$(echo "https://$(oc get route prometheus-route -n redhat-rhmi-middleware-monitoring-operator -o=jsonpath='{.spec.host}')")/api/v1/alerts
 
@@ -16,7 +19,7 @@ trap "rm tmp-alert-firing-during-perf-testing-report.csv tmp-alert-pending-durin
 
 # function to check if there are no alerts firing bar deadmansnitch
 function CHECK_NO_ALERTS(){
-    if [[ $(curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE | jq -r '.data.alerts[]| select(.state=="firing") | [.labels.alertname, .state, .activeAt ] | @csv' | wc -l) == 1 ]] ; then
+    if [[ $(curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE | jq -r '.data.alerts[]| select(.state=="firing") | [.labels.alertname, .state, .activeAt ] | @csv' | wc -l  | xargs ) == 1 ]] ; then
       echo Only alert firing is DeadMansSwitch
       date
     fi
@@ -50,14 +53,25 @@ if [ -z "$PRODUCT_NAME" ]; then
   echo - "rhsso"
   echo - "marin3r"
 else
-  echo "Check $1 product namespace and operator namespace for alerts"
+  echo "Check $PRODUCT_NAME product namespace and operator namespace for alerts"
   while :; do
-    curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE \
-    | jq -r '.data.alerts[]| select(.state=="pending" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'-operator") | [.labels.alertname, .labels.namespace, .state, .activeAt ] | @csv'>> tmp-alert-pending-during-perf-testing-report.csv
-
-    curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE \
-    | jq -r '.data.alerts[]| select(.state=="firing" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'-operator") | [.labels.alertname, .labels.namespace, .state, .activeAt ] | @csv'>> tmp-alert-firing-during-perf-testing-report.csv
-
+    if [ $PRODUCT_NAME == $RHSSO ] || [ $PRODUCT_NAME == $USER_SSO ]; then
+      curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE \
+      | jq -r '.data.alerts[]| select((.state=="pending") and (.labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'-operator" or .labels.productName=="'$PRODUCT_NAME'" or (.labels.alertname|test("Keycloak.+")))) | [.labels.alertname, .labels.namespace, .state, .activeAt ] | @csv'>> tmp-alert-pending-during-perf-testing-report.csv
+      curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE \
+      | jq -r '.data.alerts[]| select((.state=="firing") and (.labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'-operator" or .labels.productName=="'$PRODUCT_NAME'" or (.labels.alertname|test("Keycloak.+")))) | [.labels.alertname, .labels.namespace, .state, .activeAt ] | @csv'>> tmp-alert-firing-during-perf-testing-report.csv
+    elif [ $PRODUCT_NAME == $THREESCALE ]; then
+     curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE \
+     | jq -r '.data.alerts[]| select((.state=="pending") and (.labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'-operator" or .labels.productName=="'$PRODUCT_NAME'" or (.labels.alertname|test("ThreeScale.+")))) | [.labels.alertname, .labels.namespace, .state, .activeAt ] | @csv'>> tmp-alert-pending-during-perf-testing-report.csv
+     curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE \
+     | jq -r '.data.alerts[]| select((.state=="firing") and (.labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'-operator" or .labels.productName=="'$PRODUCT_NAME'" or (.labels.alertname|test("ThreeScale.+")))) | [.labels.alertname, .labels.namespace, .state, .activeAt ] | @csv'>> tmp-alert-firing-during-perf-testing-report.csv
+   else
+      curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE \
+      | jq -r '.data.alerts[]| select((.state=="pending") and (.labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'-operator" or .labels.productName=="'$PRODUCT_NAME'")) | [.labels.alertname, .labels.namespace, .state, .activeAt ] | @csv'>> tmp-alert-pending-during-perf-testing-report.csv
+      curl -s -H "Authorization: Bearer $TOKEN" $MONITORING_ROUTE \
+      | jq -r '.data.alerts[]| select((.state=="firing") and (.labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'" or .labels.namespace=="redhat-rhmi-'$PRODUCT_NAME'-operator" or .labels.productName=="'$PRODUCT_NAME'")) | [.labels.alertname, .labels.namespace, .state, .activeAt ] | @csv'>> tmp-alert-firing-during-perf-testing-report.csv
+    fi
+    set -
     # sort command to remove duplicate alert
     sort -t, -k1 -u tmp-alert-pending-during-perf-testing-report.csv > ${PRODUCT_NAME}-alert-pending-during-perf-testing-report.csv
     sort -t, -k1 -u tmp-alert-firing-during-perf-testing-report.csv > ${PRODUCT_NAME}-alert-firing-during-perf-testing-report.csv
