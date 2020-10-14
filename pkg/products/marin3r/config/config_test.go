7c55568f98d4689aef1639c1d8029f63ed705673
Sergio Franco <sfrancog@redhat.com>
2020-10-14 13:58:24 +0100

    test: Add unit test for rate limit alert configuration

--- pkg/products/marin3r/config/config_test.go
+++ pkg/products/marin3r/config/config_test.go
@@ -74,6 +74,78 @@ func TestGetRateLimitConfig(t *testing.T) {
 	}
 }
 
+func TestGetAlertConfig(t *testing.T) {
+	scheme := testScheme()
+
+	scenarios := []struct {
+		Name        string
+		InitialObjs []runtime.Object
+		Namespace   string
+		Assert      func(client.Client, map[string]*AlertConfig, error) error
+	}{
+		{
+			Name:      "Success",
+			Namespace: "redhat-test-operator",
+			InitialObjs: []runtime.Object{
+				&corev1.ConfigMap{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "rate-limit-alerts",
+						Namespace: "redhat-test-operator",
+					},
+					Data: map[string]string{
+						"alerts": `
+						{
+							"alert-1": {
+								"ruleName": "Rule1",
+								"level": "warning",
+								"minRate": "80%",
+								"maxRate": "90%",
+								"period": "2h"
+							}
+						}
+						`,
+					},
+				},
+			},
+			Assert: func(c client.Client, config map[string]*AlertConfig, err error) error {
+				if err != nil {
+					return fmt.Errorf("Unexpected error: %v", err)
+				}
+
+				alertConfig, ok := config["alert-1"]
+				if !ok {
+					return fmt.Errorf("expected key alert-1 not found in resulting config")
+				}
+
+				expectedConfig := &AlertConfig{
+					RuleName: "Rule1",
+					Level:    "warning",
+					MaxRate:  "90%",
+					MinRate:  "80%",
+					Period:   "2h",
+				}
+
+				if !reflect.DeepEqual(alertConfig, expectedConfig) {
+					return fmt.Errorf("Obtained invalid config. Expected %v, but got %v", expectedConfig, alertConfig)
+				}
+
+				return nil
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.Name, func(t *testing.T) {
+			client := fake.NewFakeClientWithScheme(scheme, scenario.InitialObjs...)
+			config, err := GetAlertConfig(context.TODO(), client, scenario.Namespace)
+
+			if err := scenario.Assert(client, config, err); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
+
 func testScheme() *runtime.Scheme {
 	scheme := runtime.NewScheme()
 	corev1.AddToScheme(scheme)
