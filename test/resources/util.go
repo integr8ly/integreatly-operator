1a679145e8bb47531121a73d8d0824b066c084e3
Phil Brookes <phil.brookes@gmail.com>
2020-12-01 09:20:17 +0000

    remove noisy logs

--- test/resources/util.go
+++ test/resources/util.go
@@ -2,14 +2,11 @@ package resources
 
 import (
 	integreatlyv1alpha1 "github.com/integr8ly/integreatly-operator/pkg/apis/integreatly/v1alpha1"
-	"github.com/sirupsen/logrus"
 )
 
 func RunningInProw(inst *integreatlyv1alpha1.RHMI) bool {
 	if v, ok := inst.Annotations["in_prow"]; !ok || v == "false" {
-		logrus.Info("detected that this operator is not running in prow")
 		return false
 	}
-	logrus.Info("detected that this operator is running in prow")
 	return true
 }
