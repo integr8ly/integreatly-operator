93dc2cfb380e29949b63415b898f8ce570a69c64
Delorean <cloud-services-delorean@redhat.com>
2020-12-08 17:00:40 +0000

    prepare for release rhoam-v1.0.1-rc1

--- version/version.go
+++ version/version.go
@@ -13,7 +13,7 @@ const (
 
 var (
 	version           = "2.7.0"
-	managedAPIVersion = "1.0.0"
+	managedAPIVersion = "1.0.1"
 )
 
 func VerifyProductAndOperatorVersion(product integreatlyv1alpha1.RHMIProductStatus, expectedProductVersion string, expectedOpVersion string) bool {
