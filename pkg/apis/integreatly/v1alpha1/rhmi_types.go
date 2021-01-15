0e9acada12608235a2b2b396511901964d937fb3
Automatron <github.eng@feedhenry.com>
2021-01-15 11:03:01 +0000

    Update 3scale manifests (0.7.0)
    Image: registry-proxy.engineering.redhat.com/rh-osbs/3scale-amp2-3scale-rhel7-operator-metadata:2.10.0-20
    Image tag: 2.10.0-20
    Image source: git://pkgs.devel.redhat.com/containers/3scale-operator-prod-operator-metadata#b76ac694ffd5a88179d96a4eaf727ca94d4a1ca3

--- pkg/apis/integreatly/v1alpha1/rhmi_types.go
+++ pkg/apis/integreatly/v1alpha1/rhmi_types.go
@@ -101,7 +101,7 @@ var (
 	OperatorVersionRHSSOUser        OperatorVersion = "11.0.3"
 
 	OperatorVersionCodeReadyWorkspaces OperatorVersion = "2.1.1"
-	OperatorVersion3Scale              OperatorVersion = "0.6.1"
+	OperatorVersion3Scale              OperatorVersion = "0.7.0"
 	OperatorVersionFuse                OperatorVersion = "1.6.0"
 	OperatorVersionCloudResources      OperatorVersion = "0.23.0"
 	OperatorVersionUPS                 OperatorVersion = "0.5.0"
