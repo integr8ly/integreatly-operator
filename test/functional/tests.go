f956d88fbfade568421f05e52d2bff5eea104663
Jim Fitzpatrick <jfitzpat@redhat.com>
2021-02-18 15:40:18 +0000

    Remove N06 Test Case
    N06 was a test done for upgrading from rhmi 2.4.x to newer. This test case is not longer valid

--- test/functional/tests.go
+++ test/functional/tests.go
@@ -8,7 +8,6 @@ var (
 		{Description: "F03 - Verify AWS elasticache resources exist and are in expected state", Test: AWSElasticacheResourcesExistTest},
 		{Description: "A21 - Verify AWS maintenance and backup windows", Test: CROStrategyOverrideAWSResourceTest},
 		{Description: "A25 - Verify standalone RHMI VPC exists and is configured properly", Test: TestStandaloneVPCExists},
-		{Description: "N06 - Verify Legacy cluster VPC is configured properly", Test: TestLegacyClusterVPC},
 		{Description: "F04 - Verify AWS s3 blob storage resources exist", Test: TestAWSs3BlobStorageResourcesExist},
 	}
 	MULTIAZ_TESTS = []common.TestCase{
