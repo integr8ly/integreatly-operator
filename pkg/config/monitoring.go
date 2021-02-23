fd72096eaf050fcfb40999b9d457c0e14bdf65b5
Jack Delahunt <jdelahun.redhat.com>
2021-02-23 15:38:35 +0000

    added cro-resources to managed API template list

--- pkg/config/monitoring.go
+++ pkg/config/monitoring.go
@@ -41,6 +41,7 @@ var managedAPITemplateList = []string{
 	"resources-by-pod",
 	"cluster-resources",
 	"critical-slo-managed-api-alerts",
+	"cro-resources",
 }
 
 func NewMonitoring(config ProductConfig) *Monitoring {
