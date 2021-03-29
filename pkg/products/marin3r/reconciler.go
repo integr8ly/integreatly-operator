28552887b97fd91808d9c01bb294f92088756b70
Laura Fitzgerald <lfitzger@redhat.com>
2021-03-29 17:02:12 +0100

    update sku secret param name to add-managed-api-service update sku configure function to allow for multiple containers update sku configure function to return immediately when isupdated is false udpate rate limit deployment reconcile to use configure fuction including update to not overwrite replicas/resources in the case of no sku change event update getsku logic to allow for sku change during a pre-existing sku change that has not fully completed return error if no secret param is found as this will become required by the add-on, dev default is handled by make cluster/prepare/sku

--- pkg/products/marin3r/reconciler.go
+++ pkg/products/marin3r/reconciler.go
@@ -106,7 +106,7 @@ func NewReconciler(configManager config.ConfigReadWriter, installation *integrea
 	}, nil
 }
 
-func (r *Reconciler) Reconcile(ctx context.Context, installation *integreatlyv1alpha1.RHMI, product *integreatlyv1alpha1.RHMIProductStatus, client k8sclient.Client, _ sku.ProductConfig) (integreatlyv1alpha1.StatusPhase, error) {
+func (r *Reconciler) Reconcile(ctx context.Context, installation *integreatlyv1alpha1.RHMI, product *integreatlyv1alpha1.RHMIProductStatus, client k8sclient.Client, productConfig sku.ProductConfig) (integreatlyv1alpha1.StatusPhase, error) {
 	r.log.Info("Start marin3r reconcile")
 
 	operatorNamespace := r.Config.GetOperatorNamespace()
@@ -210,7 +210,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, installation *integreatlyv1a
 
 	phase, err = NewRateLimitServiceReconciler(r.RateLimitConfig, installation, productNamespace, externalRedisSecretName).
 		WithStatsdConfig(statsdConfig).
-		ReconcileRateLimitService(ctx, client)
+		ReconcileRateLimitService(ctx, client, productConfig)
 	if err != nil {
 		events.HandleError(r.recorder, installation, phase, "Failed to reconcile rate limit service", err)
 		return phase, err
@@ -298,7 +298,7 @@ func (r *Reconciler) reconcileAlerts(ctx context.Context, client k8sclient.Clien
 }
 
 func (r *Reconciler) reconcileRedis(ctx context.Context, client k8sclient.Client) (integreatlyv1alpha1.StatusPhase, error) {
-	r.log.Info("Creating backend redis instance in marine3r reconcile")
+	r.log.Info("Creating backend redis instance in marin3r reconcile")
 
 	ns := r.installation.Namespace
 
