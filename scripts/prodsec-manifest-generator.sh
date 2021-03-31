6b9e72558f584a21b3c3b04e0005985b224a5785
MStokluska <mstoklus@redhat.com>
2021-03-31 12:09:45 +0100

    rhoam and rhmi prodsec manifests updates

--- scripts/prodsec-manifest-generator.sh
+++ scripts/prodsec-manifest-generator.sh
@@ -1,82 +1,123 @@
 #!/usr/bin/env bash
 
-# This script generates a manifest of the integreatly-operator which includes the direct and indirect dependencies 
-# used. The generated manifest corresponds to a released version and is located in : ../rhoam-manifests/
-
 # Compares current manifest vs manifest generated for master
 manifest_compare() {
-    # Additional sorting of the master manifest is required as it seems that prow sorts files in a different manner.
-    sort -u "$RHOAM_CURRENT_MASTER" > "$RHOAM_SORTED_MASTER"
-
-    MANIFESTS_DIFF=$(diff --suppress-common-lines ${RHOAM_MASTER_FROM_BRANCH} ${RHOAM_SORTED_MASTER})
+    sort $FILE_NAME > $SORTED_FROM_BRANCH
+    sort $CURRENT_MASTER > $SORTED_CURRENT_MASTER
+    MANIFESTS_DIFF=$(diff --suppress-common-lines ${SORTED_FROM_BRANCH} ${SORTED_CURRENT_MASTER})
     if [ ! -z "$MANIFESTS_DIFF" ]; then
-        echo "Difference found between master manifests, run `TYPE_OF_MANIFEST=master make manifest/prodsec` and push PR again"
-        
-        # Delete sorted files
-        rm -f "$RHOAM_MASTER_FROM_BRANCH"
-        rm -f "$RHOAM_SORTED_MASTER"
-        exit 1
-    else
-        echo "No difference found between the manifests"
-
-        # Delete sorted files
-        rm -f "$RHOAM_MASTER_FROM_BRANCH"
-        rm -f "$RHOAM_SORTED_MASTER"
+        echo "Difference found between master manifests"
+        rm -f "$FILE_NAME"
+        rm -f "$SORTED_FROM_BRANCH"
+        rm -f "$SORTED_CURRENT_MASTER"
         exit 0
+    elif [ -z "$MANIFESTS_DIFF" ]; then
+        echo "No difference found between the manifests"
+        rm -f "$FILE_NAME"
+        rm -f "$SORTED_FROM_BRANCH"
+        rm -f "$SORTED_CURRENT_MASTER"
+        exit 0
+    else 
+        exit 1
     fi
 }
 
 # Generates the manifest, it can be either master or production manifest
 manifest_generate() {
-    # Pre-filetered manifest file
-    PRE_SORTED_FILE="prodsec-manifests/pre-sorted-file.txt"
+    FILEPATH="${CURRENT_DIR}/products/products.yaml"
+    PRODUCTS=$(yq r -j ./products/products.yaml | jq -r '.products' | jq length)
+    # Generation manifest file
+    GENERATION_FILE="${CURRENT_DIR}/prodsec-manifests/generation-file.txt"
+
+    # Scan repos from the products.yaml file
+    for (( i=0; i<=$PRODUCTS; i++))
+    do
+        PRODUCT_NAME=$(yq r ${FILEPATH} "products[$i].name")
+        PRODUCT_VERSION=$(yq r ${FILEPATH} "products[$i].version")
+        PRODUCT_URL=$(yq r ${FILEPATH} "products[$i].url")
+
+        if [[ $(yq r ${FILEPATH} "products[$i].installType") == *"$OLM_TYPE"* ]]; then
+          TMP_DIR=$(mktemp -d)
+          echo "Generating manifest for ${PRODUCT_NAME} - version ${PRODUCT_VERSION}"
+          git clone -c advice.detachedHead=false --quiet --depth 1 -b ${PRODUCT_VERSION} ${PRODUCT_URL} ${TMP_DIR}
+          cd ${TMP_DIR}
+          go mod graph | cut -d " " -f 2 | tr @ - | while read x; do echo "${SERVICE_NAME}:${VERSION}/$PRODUCT_NAME:$PRODUCT_VERSION/$x" >> "$GENERATION_FILE"; done
+          cd ${CURRENT_DIR}
+          rm -rf $TMP_DIR
+        fi
+    done
 
     # Dependencies used
-    go mod graph | cut -d " " -f 2 | tr @ - | while read x; do echo "${SERVICE_NAME}:${VERSION}/$x" >> "$PRE_SORTED_FILE"; done
+    go mod graph | cut -d " " -f 2 | tr @ - | while read x; do echo "${SERVICE_NAME}:${VERSION}/$x" >> "$GENERATION_FILE"; done
 
-    # Remove repeating dependencies
-    sort -u "$PRE_SORTED_FILE" > "prodsec-manifests/$FILE_NAME"
+    cat "$GENERATION_FILE" > "$FILE_NAME"
 
-    # Delete pre-sorted file
-    rm -f "$PRE_SORTED_FILE"
+    echo "Manifest generated successfully, deleting temporary files"
+    rm -f "$GENERATION_FILE"
 }
 
-
+CURRENT_DIR=$(pwd)
 
 case $TYPE_OF_MANIFEST in
 "master")
-    SERVICE_NAME="services-rhoam"
     VERSION="master"
-    FILE_NAME="rhoam-master-manifest.txt"
-
+    case $OLM_TYPE in
+      "integreatly-operator")
+        OLM_TYPE="rhmi"
+        FILE_NAME="${CURRENT_DIR}/prodsec-manifests/rhmi-master-manifest.txt"
+        SERVICE_NAME="services-rhmi"
+        ;;
+      "managed-api-service")
+        OLM_TYPE="rhoam"
+        FILE_NAME="${CURRENT_DIR}/prodsec-manifests/rhoam-master-manifest.txt"
+        SERVICE_NAME="services-rhoam"
+        ;;
+      *)
+        echo "Invalid OLM_TYPE set"
+        echo "Use \"integreatly-operator\" or \"managed-api-service\""
+        exit 1
+        ;;
+    esac
     manifest_generate
-    exit 0
     ;;
 "compare")
-    SERVICE_NAME="services-rhoam"
-    VERSION="master"
-    FILE_NAME="master-from-branch-manifest.txt"
-
-    RHOAM_MASTER_FROM_BRANCH="prodsec-manifests/master-from-branch-manifest.txt"
-    RHOAM_CURRENT_MASTER="prodsec-manifests/rhoam-master-manifest.txt"
-    RHOAM_SORTED_MASTER="prodsec-manifests/rhoam-master-sorted-manifest.txt"
-
+      VERSION="master"
+      FILE_NAME="${CURRENT_DIR}/prodsec-manifests/master-from-branch-manifest.txt"
+      SORTED_FROM_BRANCH="${CURRENT_DIR}/prodsec-manifests/sorted-master-from-branch-manifest.txt"
+        case $OLM_TYPE in
+      "integreatly-operator")
+        OLM_TYPE="rhmi"
+        SERVICE_NAME="services-rhmi"
+        CURRENT_MASTER="${CURRENT_DIR}/prodsec-manifests/rhmi-master-manifest.txt"
+        SORTED_CURRENT_MASTER="${CURRENT_DIR}/prodsec-manifests/rhmi-sorted-master-manifest.txt"
+        ;;
+      "managed-api-service")
+        OLM_TYPE="rhoam"
+        SERVICE_NAME="services-rhoam"
+        CURRENT_MASTER="${CURRENT_DIR}/prodsec-manifests/rhoam-master-manifest.txt"
+        SORTED_CURRENT_MASTER="${CURRENT_DIR}/prodsec-manifests/rhoam-sorted-master-manifest.txt"
+        ;;
+      *)
+        echo "Invalid OLM_TYPE set"
+        echo "Use \"integreatly-operator\" or \"managed-api-service\""
+        exit 1
+        ;;
+    esac
     manifest_generate
     manifest_compare
-    exit 0
     ;;
 "production")
     case $OLM_TYPE in
       "integreatly-operator")
         OLM_TYPE="rhmi"
-        VERSION=$(grep integreatly-operator deploy/olm-catalog/integreatly-operator/integreatly-operator.package.yaml | awk -F v '{print $2}')
-        FILE_NAME="rhmi-production-release-manifest.txt"
+        VERSION=$(grep integreatly-operator ${CURRENT_DIR}/packagemanifests/integreatly-operator/integreatly-operator.package.yaml | awk -F v '{print $2}')
+        FILE_NAME="${CURRENT_DIR}/prodsec-manifests/rhmi-production-release-manifest.txt"
         SERVICE_NAME="services-rhmi"
         ;;
       "managed-api-service")
         OLM_TYPE="rhoam"
-        VERSION=$(grep managed-api-service deploy/olm-catalog/managed-api-service/managed-api-service.package.yaml | awk -F v '{print $3}')
-        FILE_NAME="rhoam-production-release-manifest.txt"
+        VERSION=$(grep managed-api-service ${CURRENT_DIR}/packagemanifests/managed-api-service/managed-api-service.package.yaml | awk -F v '{print $3}')
+        FILE_NAME="${CURRENT_DIR}/prodsec-manifests/rhoam-production-release-manifest.txt"
         SERVICE_NAME="services-rhoam"
         ;;
       *)
