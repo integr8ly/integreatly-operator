fee8198e9728a120214789b5a70eaa4021128fcb
Jim Fitzpatrick <jfitzpat@redhat.com>
2021-04-06 10:08:23 +0100

    Moving operator to use go version 1.16.2

--- make/functional.mk
+++ make/functional.mk
@@ -15,4 +15,4 @@ image/functional/build/push: image/functional/build image/functional/push
 
 .PHONY: test/compile/functional
 test/compile/functional:
-	CGO_ENABLED=0 go test -v -c -o integreatly-operator-test-harness.test ./test/functional
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go test -v -c -o integreatly-operator-test-harness.test ./test/functional
