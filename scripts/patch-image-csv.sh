a3f23b43ce6dd933801ac88dedc3f0de2d0ebf83
Austin Cunningham <aucunnin@redhat.com>
2021-05-13 09:50:51 +0100

    add automated checking for csv and cr before patching

--- scripts/patch-image-csv.sh
+++ scripts/patch-image-csv.sh
@@ -6,15 +6,36 @@
 # VERSION=<your image tag>
 # ORG=<you quay org>
 # ADDON_VERSION=<version set https://gitlab.cee.redhat.com/service/managed-tenants/-/blob/main/addons/managed-api-service/metadata/stage/addon.yaml#L25>
-# Set the varse.g.
+# USE_CLUSTER_STORAGE=<true/false optional will default true if not set>
+# Set the vars.g.
 # export VERSION=v1.6.0
 # export ADDON_VERSION=v1.6.0
 # export ORG=austincunningham
+# export USE_CLUSTER_STORAGE=false
 # ./patch-image-csv.sh
+#
+# Check csv exists
+oc get csv managed-api-service.$VERSION -n redhat-rhoam-operator
+while [ $? -ne 0 ]; do
+  oc get csv managed-api-service.$VERSION -n redhat-rhoam-operator
+done
+# Patch the csv
 oc patch ClusterServiceVersion managed-api-service.$ADDON_VERSION -n redhat-rhoam-operator --patch '{"metadata": {"annotations": {"containerImage": "quay.io/'$ORG'/managed-api-service:'$VERSION'" }}}' --type=merge
 oc patch ClusterServiceVersion managed-api-service.$ADDON_VERSION -n redhat-rhoam-operator --type='json' -p='[{"op": "replace", "path": "/spec/install/spec/deployments/0/spec/template/spec/containers/0/image", "value":"quay.io/'$ORG'/managed-api-service:'$VERSION'"}]'
 echo Verificaton the CSV has been patched :
 oc get csv managed-api-service.$VERSION -n redhat-rhoam-operator -o yaml | grep "$ORG"
 echo " "
-echo You will have to patch the RHMI CR when its created with useClusterStorge e.g.
-echo oc patch rhmi rhoam -n redhat-rhoam-operator --patch \'{ \"spec\": {\"useClusterStorage\": \"true\" }}\' --type=merge
+echo Patching the RHMI CR when its created with useClusterStorge
+# Check the rhmi cr exists
+oc get rhmi rhoam -n redhat-rhoam-operator
+while [ $? -ne 0 ]; do
+  oc get rhmi rhoam -n redhat-rhoam-operator
+done
+# default USE_CLUSTER_STORAGE to true if not set
+if [ -n "$USE_CLUSTER_STORAGE"]
+  USE_CLUSTER_STORAGE=true
+fi
+# Patch rhmi CR
+oc patch rhmi rhoam -n redhat-rhoam-operator --patch '{ "spec": {"useClusterStorage": "'$USE_CLUSTER_STORAGE'" }}\' --type=merge
+echo Verification USE_CLUSTER_STORAGE is set :
+oc get rhmi rhoam -n redhat-rhoam-operator -o yaml | grep useClusterStorage
