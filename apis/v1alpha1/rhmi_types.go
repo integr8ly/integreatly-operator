23626849d136b3ac42ce6afe04453098646195e3
Valery Mogilevsky <vmogilev@redhat.com>
2021-06-17 15:55:51 +0300

    MGDAPI-2072 - Address high CVEs discovered in grafana-operator image (used in RHOAM v1.8.0)

--- apis/v1alpha1/rhmi_types.go
+++ apis/v1alpha1/rhmi_types.go
@@ -96,7 +96,7 @@ var (
 	VersionMonitoringSpec      ProductVersion = "1.0"
 	VersionSolutionExplorer    ProductVersion = "2.28.0"
 	VersionMarin3r             ProductVersion = "0.7.0"
-	VersionGrafana             ProductVersion = "3.10.1"
+	VersionGrafana             ProductVersion = "3.10.2"
 
 	// Versioning for Fuse on OpenShift does not follow a similar pattern to other products.
 	// It is currently implicitly tied to version 7.6 of Fuse, hence the 7.6 value for VersionFuseOnOpenshift above
@@ -126,7 +126,7 @@ var (
 	OperatorVersionApicurito           OperatorVersion = "1.6.0"
 	OperatorVersionMonitoringSpec      OperatorVersion = "1.0"
 	OperatorVersionMarin3r             OperatorVersion = "0.7.0"
-	OperatorVersionGrafana             OperatorVersion = "3.10.1"
+	OperatorVersionGrafana             OperatorVersion = "3.10.2"
 
 	// Event reasons to be used when emitting events
 	EventProcessingError       string = "ProcessingError"
