ec029487e872a07fd56a31ab54c4b71824fd8eca
jackdelahunt <jdelahun@redhat.com>
2021-07-01 14:46:30 +0100

    Update: Updated AMO to v1.6.1

--- apis/v1alpha1/rhmi_types.go
+++ apis/v1alpha1/rhmi_types.go
@@ -85,7 +85,7 @@ var (
 	VersionAMQStreams          ProductVersion = "1.1.0"
 	VersionCodeReadyWorkspaces ProductVersion = "2.1.1"
 	VersionFuseOnOpenshift     ProductVersion = "7.6"
-	VersionMonitoring          ProductVersion = "1.6.0"
+	VersionMonitoring          ProductVersion = "1.6.1"
 	Version3Scale              ProductVersion = "2.10.0"
 	VersionUps                 ProductVersion = "2.3.2"
 	VersionCloudResources      ProductVersion = "0.28.0"
@@ -112,7 +112,7 @@ var (
 	// Operator image tags
 	OperatorVersionAMQStreams       OperatorVersion = "1.1.0"
 	OperatorVersionAMQOnline        OperatorVersion = "1.4"
-	OperatorVersionMonitoring       OperatorVersion = "1.6.0"
+	OperatorVersionMonitoring       OperatorVersion = "1.6.1"
 	OperatorVersionSolutionExplorer OperatorVersion = "0.0.62"
 	OperatorVersionRHSSO            OperatorVersion = "14.0.0"
 	OperatorVersionRHSSOUser        OperatorVersion = "14.0.0"
