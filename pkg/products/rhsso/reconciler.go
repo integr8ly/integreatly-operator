4f082368f3abb082707eb8ab6f7d1e47e06682fc
MStokluska <mstoklus@redhat.com>
2021-07-30 11:23:26 +0100

    wip

--- pkg/products/rhsso/reconciler.go
+++ pkg/products/rhsso/reconciler.go
@@ -310,9 +310,11 @@ func (r *Reconciler) reconcileComponents(ctx context.Context, installation *inte
 			return fmt.Errorf("failed to setup Openshift IDP: %w", err)
 		}
 
-		err = r.setupGithubIDP(ctx, kc, kcr, serverClient, installation)
-		if err != nil {
-			return fmt.Errorf("failed to setup Github IDP: %w", err)
+		if !integreatlyv1alpha1.IsRHOAMMultitenant(integreatlyv1alpha1.InstallationType(installation.Spec.Type)) {
+			err = r.setupGithubIDP(ctx, kc, kcr, serverClient, installation)
+			if err != nil {
+				return fmt.Errorf("failed to setup Github IDP: %w", err)
+			}
 		}
 		return nil
 	})
@@ -448,9 +450,19 @@ func getUserDiff(keycloakUsers []keycloak.KeycloakAPIUser, openshiftUsers []user
 }
 
 func syncronizeWithOpenshiftUsers(ctx context.Context, keycloakUsers []keycloak.KeycloakAPIUser, serverClient k8sclient.Client, ns string) ([]keycloak.KeycloakAPIUser, error) {
-	openshiftUsers, err := userHelper.GetUsersInActiveIDPs(ctx, serverClient)
-	if err != nil {
-		return nil, errors.Wrap(err, "could not get users in active IDPs")
+	var openshiftUsers *usersv1.UserList
+	var err error
+
+	if integreatlyv1alpha1.IsRHOAMMultitenant(integreatlyv1alpha1.InstallationType(installation.Spec.Type)) {
+		openshiftUsers, err = userHelper.GetMultiTenantUsers(ctx, serverClient)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not get users in active IDPs")
+		}
+	} else {
+		openshiftUsers, err = userHelper.GetUsersInActiveIDPs(ctx, serverClient)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not get users in active IDPs")
+		}
 	}
 
 	groups := &usersv1.GroupList{}
@@ -499,7 +511,7 @@ func syncronizeWithOpenshiftUsers(ctx context.Context, keycloakUsers []keycloak.
 		return nil, err
 	}
 	for index := range keycloakUsers {
-		keycloakUsers[index].ClientRoles = getKeycloakRoles()
+		keycloakUsers[index].ClientRoles = getKeycloakRoles(integreatlyv1alpha1.InstallationType(installation.Spec.Type))
 	}
 
 	return keycloakUsers, nil
@@ -602,15 +614,24 @@ func createAuthDelayAuthenticationFlow(authenticated keycloakCommon.KeycloakInte
 	return nil
 }
 
-func getKeycloakRoles() map[string][]string {
-	roles := map[string][]string{
-		"account": {
-			"manage-account",
-			"view-profile",
-		},
-		"broker": {
-			"read-token",
-		},
+func getKeycloakRoles(installationType integreatlyv1alpha1.InstallationType) map[string][]string {
+	var roles map[string][]string
+	if integreatlyv1alpha1.IsRHOAMMultitenant(integreatlyv1alpha1.InstallationType(installationType)) {
+		roles = map[string][]string{
+			"account": {
+				"view-profile",
+			},
+		}
+	} else {
+		roles = map[string][]string{
+			"account": {
+				"manage-account",
+				"view-profile",
+			},
+			"broker": {
+				"read-token",
+			},
+		}
 	}
 	return roles
 }
