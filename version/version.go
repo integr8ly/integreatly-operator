2b8f20621d5e53f23826ee55cb95db863f6a06dd
Delorean <cloud-services-delorean@redhat.com>
2021-08-16 13:58:22 +0000

    prepare for release rhoam-v1.11.0-rc1

--- version/version.go
+++ version/version.go
@@ -13,7 +13,7 @@ const (
 
 var (
 	version           = "2.9.0"
-	managedAPIVersion = "1.10.0"
+	managedAPIVersion = "1.11.0"
 	log               = l.NewLoggerWithContext(l.Fields{l.ComponentLogContext: "version"})
 )
 
