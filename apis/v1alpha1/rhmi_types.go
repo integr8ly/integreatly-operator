66835e999f8abca1ea8c567358ccf69b6c431eb3
Brian Gallagher <briangal@gmail.com>
2021-09-22 11:57:53 +0100

    bump grafana version

--- apis/v1alpha1/rhmi_types.go
+++ apis/v1alpha1/rhmi_types.go
@@ -97,7 +97,7 @@ var (
 	VersionMonitoringSpec      ProductVersion = "1.0"
 	VersionSolutionExplorer    ProductVersion = "2.28.0"
 	VersionMarin3r             ProductVersion = "0.8.0"
-	VersionGrafana             ProductVersion = "3.10.2"
+	VersionGrafana             ProductVersion = "3.10.4"
 
 	// Versioning for Fuse on OpenShift does not follow a similar pattern to other products.
 	// It is currently implicitly tied to version 7.6 of Fuse, hence the 7.6 value for VersionFuseOnOpenshift above
@@ -127,7 +127,7 @@ var (
 	OperatorVersionApicurito           OperatorVersion = "1.6.0"
 	OperatorVersionMonitoringSpec      OperatorVersion = "1.0"
 	OperatorVersionMarin3r             OperatorVersion = "0.8.0"
-	OperatorVersionGrafana             OperatorVersion = "3.10.2"
+	OperatorVersionGrafana             OperatorVersion = "3.10.4"
 
 	// Event reasons to be used when emitting events
 	EventProcessingError       string = "ProcessingError"
