bea4366f3c71a3028a719557bb0e132f52d54957
Brian Gallagher <briangal@gmail.com>
2021-09-26 00:22:41 +0100

    ASDF

--- test/common/LoginUsers.go
+++ test/common/LoginUsers.go
@@ -1,8 +1,8 @@
 package common
 
 import (
-	//"github.com/integr8ly/integreatly-operator/test/resources"
-	//"fmt"
+	"fmt"
+	"github.com/integr8ly/integreatly-operator/test/resources"
 	"strconv"
 )
 
@@ -11,15 +11,17 @@ func TestLoginAllUsers(t TestingTB, ctx *TestingContext) {
 	user := "test-user0"
 	//DefaultPassword := "Password1"
 
-	for i := 1; i < 10; i++ {
-		user = user + strconv.FormatInt(int64(i), 10)
+	for i := 10; i < 3000; i++ {
+		user = "test-user" + strconv.FormatInt(int64(i), 10)
 
+		httpClient, _ := NewTestingHTTPClient(ctx.KubeConfig)
 
-		masterURL := "https://console-openshift-console.apps.bg-byoc.lgqh.s1.devshift.org"
+		masterURL := "console-openshift-console.apps.bg-byoc.lgqh.s1.devshift.org"
 		// get rhmi developer user tokens
 		//if err := resources.DoAuthOpenshiftUser(fmt.Sprintf("%s/auth/login", masterURL), user, DefaultPassword, ctx.HttpClient, "devsandbox", t); err != nil {
-		//	t.Fatalf("error occured trying to get token : %v", err)
-		//}
+		if err := resources.DoAuthOpenshiftUser( fmt.Sprintf("%s/auth/login", masterURL), user, DefaultPassword, httpClient, "devsandbox", t); err != nil {
+			t.Fatalf("error occured trying to get token : %v", err)
+		}
 
 		t.Log("here !!!! ", masterURL)
 
