6d93468f7316df5fdb91513fba308d5e97938470
Delorean <cloud-services-delorean@redhat.com>
2021-11-01 09:40:49 +0000

    prepare for release rhoam-v1.14.0-rc1

--- Makefile
+++ Makefile
@@ -31,7 +31,7 @@ SANDBOX_NAMESPACE ?= sandbox-rhoam-operator
 
 # These tags are modified by the prepare-release script.
 RHMI_TAG ?= 2.9.0
-RHOAM_TAG ?= 1.13.0
+RHOAM_TAG ?= 1.14.0
 
 export SKIP_FLAKES := true
 
--- version/version.go
+++ version/version.go
@@ -13,7 +13,7 @@ const (
 
 var (
 	version           = "2.9.0"
-	managedAPIVersion = "1.13.0"
+	managedAPIVersion = "1.14.0"
 	log               = l.NewLoggerWithContext(l.Fields{l.ComponentLogContext: "version"})
 )
 
