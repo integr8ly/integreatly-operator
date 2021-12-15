a96e0656488770040f48c9072fc813094b224890
MStokluska <mstoklus@redhat.com>
2021-12-15 14:22:08 +0000

    MGDAPI-2909 address commments

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -67,7 +67,6 @@ update_base_csv() {
   yq e -i ".metadata.name=\"$OLM_TYPE.v$VERSION\"" config/manifests-$OPERATOR_TYPE/bases/$OLM_TYPE.clusterserviceversion.yaml
   yq e -i ".spec.version=\"$VERSION\"" config/manifests-$OPERATOR_TYPE/bases/$OLM_TYPE.clusterserviceversion.yaml
   if [[ "${VERSION}" != "${PREVIOUS_VERSION}" ]]; then
-    # yq e -i ".spec.replaces=\"${OLM_TYPE}.${PREVIOUS_VERSION}\"" bundles/$OLM_TYPE/${VERSION}/manifests/$OLM_TYPE.clusterserviceversion.yaml
     yq e -i ".spec.replaces=\"$OLM_TYPE.v$PREVIOUS_VERSION\"" config/manifests-$OPERATOR_TYPE/bases/$OLM_TYPE.clusterserviceversion.yaml
   fi
 }
