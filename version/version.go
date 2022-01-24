44f6b16b420441bb5dc1efc2fb6bdbf6fd99eaf0
Delorean <cloud-services-delorean@redhat.com>
2022-01-24 10:10:12 +0000

    MGDAPI-3209 prepare for release rhoam-v1.16.0-rc1

--- version/version.go
+++ version/version.go
@@ -13,7 +13,7 @@ const (
 
 var (
 	version           = "2.9.0"
-	managedAPIVersion = "1.15.2"
+	managedAPIVersion = "1.16.0"
 	log               = l.NewLoggerWithContext(l.Fields{l.ComponentLogContext: "version"})
 )
 
