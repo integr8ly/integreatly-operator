6e7e040c2ca9b05ad89c49f49ace4aafede07141
KevFan <chfan@redhat.com>
2022-03-23 10:42:50 +0000

    MGDAPI-2657 - feat: allow backup restore script to get AWS key locally for sts support

--- test/scripts/backup-restore/j08-verify-user-sso-backup-and-restore.sh
+++ test/scripts/backup-restore/j08-verify-user-sso-backup-and-restore.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
+# USAGE
+# ./j08-verify-user-sso-backup-and-restore <optional NS_PREFIX> <optional AWS_ACCESS_KEY_ID> <optional AWS_SECRET_ACCESS_KEY>
+# ^C to break
+# Tests User SSO postgres back up and restore
+#
+# AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID should be set before running the script to support STS clusters as AWS
+# AWS credentials are not available on STS clusters. By default the script will try to use the credentials on cluster.
+#
+# PREREQUISITES
+# - oc (logged in at the cmd line)
 
 # Import the test function
 . ./postgres.sh --source-only
 
 # Set the parameters
+# Use passed in AWS credentials for STS, otherwise default to getting from cluster for normal OSD. Exit if these credentials are empty
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:=$(oc get secret aws-creds -n kube-system -o jsonpath='{.data.aws_access_key_id}' | base64 --decode)}
+[[ -z "$AWS_ACCESS_KEY_ID" ]] && { echo "AWS_ACCESS_KEY_ID cannot be empty"; exit 1; }
+
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:=$(oc get secret aws-creds -n kube-system -o jsonpath='{.data.aws_secret_access_key}' | base64 --decode)}
+[[ -z "$AWS_SECRET_ACCESS_KEY" ]] && { echo "AWS_SECRET_ACCESS_KEY cannot be empty"; exit 1; }
+
 NS_PREFIX="${NS_PREFIX:=redhat-rhmi}"
-export AWS_SECRET_ACCESS_KEY=$(oc get secret aws-creds -n kube-system -o jsonpath={.data.aws_secret_access_key} | base64 --decode)
-export AWS_ACCESS_KEY_ID=$(oc get secret aws-creds -n kube-system -o jsonpath={.data.aws_access_key_id} | base64 --decode)
 AWS_DB_ID=$(oc get secret/keycloak-db-secret -o go-template --template="{{.data.POSTGRES_EXTERNAL_ADDRESS|base64decode}}" -n ${NS_PREFIX}-user-sso | awk -F\. '{print $1}')
 AWS_REGION=$(oc get infrastructure cluster -o jsonpath='{.status.platformStatus.aws.region}')
 RHMI_CR_NAME=$(oc get rhmi -n ${NS_PREFIX}-operator -o json | jq -r '.items[0].metadata.name')
