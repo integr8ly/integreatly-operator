f29e6f16cd2bab8641079e329b25c45aa8e863d5
Jim Fitzpatrick <jfitzpat@redhat.com>
2022-05-06 09:27:51 +0100

    MGDAPI-3764: Error Fix
    Add missing error assignment

--- pkg/products/threescale/reconciler.go
+++ pkg/products/threescale/reconciler.go
@@ -1672,7 +1672,7 @@ func (r *Reconciler) reconcile3scaleMultiTenancy(ctx context.Context, serverClie
 			"totalAccounts":       len(accountsToBeDeleted),
 		},
 	)
-	r.tsClient.DeleteTenants(*accessToken, accountsToBeDeleted)
+	err = r.tsClient.DeleteTenants(*accessToken, accountsToBeDeleted)
 	if err != nil {
 		r.log.Error("Error deleting tenant accounts:", err)
 		return integreatlyv1alpha1.PhaseFailed, err
