f0a5c3277a768f215337ff8b0a6cfca82a43e89c
pstefans <pstefans@redhat.com>
2022-06-01 09:51:17 +0100

    MGDAPI-4095 small update to J05

--- test/scripts/backup-restore/j05-verify-3scale-postgres-backup-and-restore.sh
+++ test/scripts/backup-restore/j05-verify-3scale-postgres-backup-and-restore.sh
@@ -5,7 +5,7 @@
 
 # Make sure we're using GNU grep
 if grep -V | grep BSD; then
-  grep_cmd=$(which ggrep)
+  grep_cmd=$(which grep)
   if [ -z "$grep_cmd" ]; then
     echo "This script requires GNU version of 'grep'. You can install it with \`brew install grep\`"
     exit 1
@@ -15,7 +15,7 @@ else
 fi
 
 # Set the parameters
-NS_PREFIX="${NS_PREFIX:=redhat-rhmi}"
+NS_PREFIX="${NS_PREFIX:=redhat-rhoam}"
 export AWS_SECRET_ACCESS_KEY=$(oc get secret aws-creds -n kube-system -o jsonpath='{.data.aws_secret_access_key}' | base64 --decode)
 export AWS_ACCESS_KEY_ID=$(oc get secret aws-creds -n kube-system -o jsonpath='{.data.aws_access_key_id}' | base64 --decode)
 AWS_DB_ID=$(oc get secret/system-database -o go-template --template="{{.data.URL|base64decode}}" -n ${NS_PREFIX}-3scale | $grep_cmd -Po "(?<=@).*?(?=\.)")
