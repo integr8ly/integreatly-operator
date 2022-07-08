2a14f59d7c94834e21b80ce80dc2c8d6f92a505b
Automatron <github.eng@feedhenry.com>
2022-07-08 11:09:27 +0100

    MGDAPI-3209 Update 3scale manifests (0.9.0)
    Image: quay.io/integreatly/3scale-index:v0.9.0
    Image tag: 0.9.0
    Image source: git://pkgs.devel.redhat.com/containers/3scale-operator-prod-operator-metadata#9cf17da90ca5cffd9404f3761829d81ad92f04fe

--- apis/v1alpha1/rhmi_types.go
+++ apis/v1alpha1/rhmi_types.go
@@ -125,7 +125,7 @@ var (
 	OperatorVersionRHSSOUser        OperatorVersion = "18.0.0"
 
 	OperatorVersionCodeReadyWorkspaces OperatorVersion = "2.1.1"
-	OperatorVersion3Scale              OperatorVersion = "0.8.4"
+	OperatorVersion3Scale              OperatorVersion = "0.9.0"
 	OperatorVersionFuse                OperatorVersion = "1.6.0"
 	OperatorVersionCloudResources      OperatorVersion = "0.39.0"
 	OperatorVersionUPS                 OperatorVersion = "0.5.0"
