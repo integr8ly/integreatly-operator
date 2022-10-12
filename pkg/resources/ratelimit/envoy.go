9f971e9c8b8fbf22f65c5e6ce4b9906f07355b48
MStokluska <mstoklus@redhat.com>
2022-10-12 10:40:28 +0100

    MGDAPI-4749 bump envoy image

--- pkg/resources/ratelimit/envoy.go
+++ pkg/resources/ratelimit/envoy.go
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	EnvoyImage      = "registry.redhat.io/openshift-service-mesh/proxyv2-rhel8:2.1.2-2"
+	EnvoyImage      = "registry.redhat.io/openshift-service-mesh/proxyv2-rhel8:2.2.3-6"
 	EnvoyAPIVersion = "v3"
 )
 
