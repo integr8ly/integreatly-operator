dfa50319b4822328600fa80620f635ed7f07b797
KevFan <chfan@redhat.com>
2023-02-01 12:14:41 +0000

    MGDAPI-5134 - observability: v4.0.4

--- apis/v1alpha1/rhmi_types.go
+++ apis/v1alpha1/rhmi_types.go
@@ -76,7 +76,7 @@ var (
 	VersionMonitoringSpec ProductVersion = "1.0"
 	VersionMarin3r        ProductVersion = "0.11.0"
 	VersionGrafana        ProductVersion = "4.2.0"
-	VersionObservability  ProductVersion = "4.0.0"
+	VersionObservability  ProductVersion = "4.0.4"
 
 	PreflightInProgress PreflightStatus = ""
 	PreflightSuccess    PreflightStatus = "successful"
@@ -92,7 +92,7 @@ var (
 	OperatorVersionMonitoringSpec OperatorVersion = "1.0"
 	OperatorVersionMarin3r        OperatorVersion = "0.11.0"
 	OperatorVersionGrafana        OperatorVersion = "4.2.0"
-	OperatorVersionObservability  OperatorVersion = "4.0.0"
+	OperatorVersionObservability  OperatorVersion = "4.0.4"
 
 	// Event reasons to be used when emitting events
 	EventProcessingError       string = "ProcessingError"
