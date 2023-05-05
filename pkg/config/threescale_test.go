207158c711e51a97e354b53573a977ea012fa142
Adam Cattermole <acatterm@redhat.com>
2023-05-05 13:16:55 +0100

    MGDAPI-5691 - set Redis size for 100M on GCP

--- pkg/config/threescale_test.go
+++ pkg/config/threescale_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/integr8ly/integreatly-operator/pkg/resources/quota"
+	configv1 "github.com/openshift/api/config/v1"
 )
 
 func TestThreeScale_GetBackendRedisNodeSize(t1 *testing.T) {
@@ -11,7 +12,8 @@ func TestThreeScale_GetBackendRedisNodeSize(t1 *testing.T) {
 		config ProductConfig
 	}
 	type args struct {
-		activeQuota string
+		activeQuota  string
+		platformType configv1.PlatformType
 	}
 	tests := []struct {
 		name   string
@@ -22,16 +24,26 @@ func TestThreeScale_GetBackendRedisNodeSize(t1 *testing.T) {
 		{
 			name: "test empty string is returned when active quota is not 100 M",
 			args: args{
-				activeQuota: quota.OneHundredThousandQuotaName,
+				activeQuota:  quota.OneHundredThousandQuotaName,
+				platformType: configv1.AWSPlatformType,
 			},
 			want: "",
 		},
 		{
-			name: "test cache.m5.large is returned when active quota is 100 M",
+			name: "test cache.m5.large is returned when active quota is 100 M and platformType AWS",
 			args: args{
-				activeQuota: quota.OneHundredMillionQuotaName,
+				activeQuota:  quota.OneHundredMillionQuotaName,
+				platformType: configv1.AWSPlatformType,
 			},
 			want: "cache.m5.large",
+		},
+		{
+			name: "test 11 is returned when active quota is 100 M and platformType GCP",
+			args: args{
+				activeQuota:  quota.OneHundredMillionQuotaName,
+				platformType: configv1.GCPPlatformType,
+			},
+			want: "11",
 		},
 	}
 	for _, tt := range tests {
@@ -39,7 +51,7 @@ func TestThreeScale_GetBackendRedisNodeSize(t1 *testing.T) {
 			t := &ThreeScale{
 				config: tt.fields.config,
 			}
-			if got := t.GetBackendRedisNodeSize(tt.args.activeQuota); got != tt.want {
+			if got := t.GetBackendRedisNodeSize(tt.args.activeQuota, tt.args.platformType); got != tt.want {
 				t1.Errorf("GetBackendRedisNodeSize() = %v, want %v", got, tt.want)
 			}
 		})
