59ab51eb1a6494f094d716011a2e3d3aa3582d96
MStokluska <mstoklus@redhat.com>
2023-05-23 09:10:56 +0100

    MGDAPI-5732 refactor one more alert

--- pkg/products/marin3r/rejectedRequestsPrometheusRules.go
+++ pkg/products/marin3r/rejectedRequestsPrometheusRules.go
@@ -12,16 +12,8 @@ import (
 
 const rejectedRequestsAlertExpr = "abs(clamp_min(increase(limited_calls[1m]) - %f, 0) / (sum(increase(authorized_calls[1m])) + sum(increase(limited_calls[1m]))) - (increase(limited_calls[1m]) / (sum(increase(authorized_calls[1m])) + sum(increase(limited_calls[1m]))))) > 0.3"
 
-func (r *Reconciler) newRejectedRequestsAlertsReconciler(logger l.Logger, installType string) (resources.AlertReconciler, error) {
+func (r *Reconciler) newRejectedRequestsAlertsReconciler(logger l.Logger, installType, ns string) (resources.AlertReconciler, error) {
 	installationName := resources.InstallationNames[installType]
-
-	observabilityConfig, err := r.ConfigManager.ReadObservability()
-	if err != nil {
-		logger.Warning("failed to get observability config")
-		return nil, err
-	}
-
-	namespace := observabilityConfig.GetNamespace()
 	alertName := "marin3r-rejected-requests"
 
 	limitPerMinute, err := config.ConvertRate(
@@ -41,7 +33,7 @@ func (r *Reconciler) newRejectedRequestsAlertsReconciler(logger l.Logger, instal
 			{
 				AlertName: alertName,
 				GroupName: "rejected-requests.rules",
-				Namespace: namespace,
+				Namespace: ns,
 				Rules: []monv1.Rule{
 					{
 						Alert: "RHOAMApiUsageRejectedRequestsMismatch",
