60fc48ff11ff728984027cffc6bd4f5362de7193
Adam Cattermole <acatterm@redhat.com>
2023-05-24 14:48:06 +0100

    MGDAPI-5753 - update mcg operator to v4.12.3

--- apis/v1alpha1/rhmi_types.go
+++ apis/v1alpha1/rhmi_types.go
@@ -74,7 +74,7 @@ var (
 	VersionRHSSOUser      ProductVersion = "7.6"
 	VersionMonitoringSpec ProductVersion = "1.0"
 	VersionMarin3r        ProductVersion = "0.11.0"
-	VersionMCG            ProductVersion = "4.11.4"
+	VersionMCG            ProductVersion = "4.12.3-rhodf"
 	VersionGrafana        ProductVersion = "4.2.0"
 	VersionObservability  ProductVersion = "4.2.1"
 
@@ -93,7 +93,7 @@ var (
 	OperatorVersionMarin3r        OperatorVersion = "0.11.0"
 	OperatorVersionGrafana        OperatorVersion = "4.2.0"
 	OperatorVersionObservability  OperatorVersion = "4.2.1"
-	OperatorVersionMCG            OperatorVersion = "4.11.4"
+	OperatorVersionMCG            OperatorVersion = "4.12.3-rhodf"
 
 	// Event reasons to be used when emitting events
 	EventProcessingError       string = "ProcessingError"
